3eca490e6cddc9651517a2f60419de52f70e53a2
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-01-30 18:26:13 +0100

    test(dice.xlsx): upd splitted veh-family-id on test-file, add input.xlsx

--- tests/sampling/__init__.py
+++ tests/sampling/__init__.py
@@ -8,5 +8,6 @@
 import os.path as osp
 
 mydir = osp.dirname(__file__)
-test_inp_fpath = osp.join(mydir, '..', '..', 'co2mpas', 'demos', 'co2mpas_demo-0.xlsx')
+test_inp_fpath = osp.join(mydir, 'input.xlsx')
 test_out_fpath = osp.join(mydir, 'output.xlsx')
+test_vfid = 'RL-99-BM3-2017-0001'
