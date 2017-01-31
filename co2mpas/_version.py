71f1dc2d973249b2fe4ec11962b434c0e9c70e6b
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-02-01 00:51:59 +0100

    chore(ver): bump v1.5.0.dev2-->dev3 for rc0 AIO.

--- co2mpas/_version.py
+++ co2mpas/_version.py
@@ -9,7 +9,7 @@
 
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "1.5.0.dev2"  # Also update README.rst, doc/allinone.rst, CHANGES.rst,
+__version__ = version = "1.5.0.dev3"  # Also update README.rst, doc/allinone.rst, CHANGES.rst,
 
 #: Input/Output file's version.
 __file_version__ = "2.2.5"
@@ -21,4 +21,4 @@ __dice_report_version__ = '0.0.2'
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
 #: Release date.
-__updated__ = "2017-01-31 01:26:05"
+__updated__ = "2017-02-01 00:50:03"
