a99475643f320409e1c4dfc7a0a4c88a5ee0fb97
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-02-01 01:15:47 +0100

    chore(deps): fix egg-metadata for temporary traitslets@ankostis-dev

--- setup.py
+++ setup.py
@@ -188,11 +188,11 @@ setup(
         'Pillow',           ## for tkui
         'toolz',
         'schedula>=0.1.1',
-        'traitlets>=5.0.0dev',
+        'traitlets>=5.0.0.dev',
         'python-gnupg',
     ],
     dependency_links=[
-        'https://github.com/ankostis/traitlets/tarball/dev#egg=traitlets-5.0.0dev',
+        'https://github.com/ankostis/traitlets/tarball/dev#egg=traitlets-5.0.0.dev',
     ],
     extras_require={
         'dice': [
