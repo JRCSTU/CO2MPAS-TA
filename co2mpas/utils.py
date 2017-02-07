e958aecce55b35c54ee33d3be38af3ae437627a6
arcidvi <vinci1it2000@gmail.com>
2017-02-07 16:27:54 +0100

    fix(utils), #236: Remove unused functions.

--- co2mpas/utils.py
+++ co2mpas/utils.py
@@ -17,7 +17,6 @@ from collections import OrderedDict, defaultdict
 from contextlib import contextmanager
 import inspect
 import io
-import math
 import re
 import statistics
 import sys
@@ -34,8 +33,7 @@ except AttributeError:
 
 __all__ = [
     'grouper', 'sliding_window', 'median_filter', 'reject_outliers',
-    'bin_split', 'interpolate_cloud', 'clear_fluctuations', 'argmax',
-    'derivative'
+    'clear_fluctuations', 'argmax', 'derivative'
 ]
 
 
@@ -241,155 +239,6 @@ def ret_v(v):
     """
 
     return lambda: v
-
-
-def bin_split(x, bin_std=(0.01, 0.1), n_min=None, bins_min=None):
-    """
-    Splits the input data with variable bins.
-
-    :param x:
-        Input data.
-    :type x: Iterable
-
-    :param bin_std:
-        Bin standard deviation limits.
-    :type bin_std: (float, float)
-
-    :param n_min:
-        Minimum number of data inside a bin [-].
-    :type n_min: int
-
-    :param bins_min:
-        Minimum number of bins [-].
-    :type bins_min: int
-
-    :return:
-        Bins and their statistics.
-    :rtype: (list, list)
-    """
-
-    x = np.asarray(x)
-    edges = [x.min(), x.max() + sys.float_info.epsilon * 2]  # initial edge.
-
-    max_bin_size = edges[1] - edges[0]  #
-    min_bin_size = max_bin_size / len(x)
-    if n_min is None:
-        n_min = math.sqrt(len(x))
-
-    if bins_min is not None:
-        max_bin_size /= bins_min
-    bin_stats = []
-
-    def _bin_split(x, m, std, x_min, x_max):
-        bin_size = x_max - x_min
-        n = len(x)
-
-        y0 = x[x < m]
-        y1 = x[m <= x]
-        m_y0, std_y0 = _stats(y0)
-        m_y1, std_y1 = _stats(y1)
-
-        if any(
-                [bin_size > max_bin_size,
-                 all([std > bin_std[1],
-                      x_min < m < x_max,
-                      bin_size > min_bin_size,
-                      n > n_min,
-                      (m_y1 - m_y0) / bin_size > 0.2
-                      ])
-                 ]) and (std_y0 > bin_std[0] or std_y1 > bin_std[0]):
-
-            edges.append(m)
-            _bin_split(y0, m_y0, std_y0, x_min, m)
-            _bin_split(y1, m_y1, std_y1, m, x_max)
-
-        else:
-            bin_stats.append([np.median(x), std / n, std, m, n])
-
-    def _stats(x):
-        m = np.mean(x)
-        std = np.abs(np.std(x) / m)
-        return [m, std]
-
-    _bin_split(x, *(_stats(x) + edges))
-
-    edges = sorted(edges)
-
-    bin_stats = sorted(bin_stats)
-
-    def _bin_merge(x, edges, bin_stats):
-        import schedula.utils as dsp_utl
-
-        bins = OrderedDict(enumerate(zip(dsp_utl.pairwise(edges),
-                                         bin_stats)))
-        new_edges = [edges[0]]
-        new_bin_stats = []
-
-        for k0 in range(len(bins) - 1):
-            v0, v1 = (bins[k0], bins[k0 + 1])
-            e_min, e_max = (v0[0][0], v1[0][1])
-            if (v1[1][0] - v0[1][0]) / (e_max - e_min) <= 0.33:
-                y = x[(e_min <= x) & (x < e_max)]
-                m, std = _stats(y)
-                if std < bin_std[1]:
-                    n = v0[1][-1] + v1[1][-1]
-                    bins[k0 + 1] = (
-                        (e_min, e_max), [np.median(y), std / n, std, m, n])
-                    del bins[k0]
-
-        for e, s in bins.values():
-            new_edges.append(e[1])
-            if s[2] < bin_std[1]:
-                s[2] *= s[3]
-                new_bin_stats.append(s[1:] + [s[0]])
-
-        new_bin_stats = sorted(new_bin_stats)
-        return new_edges, new_bin_stats
-
-    return _bin_merge(x, edges, bin_stats)
-
-
-# noinspection PyTypeChecker
-def interpolate_cloud(x, y):
-    """
-    Defines a function that interpolate a cloud of points.
-
-    :param x:
-        x data.
-    :type x: Iterable
-
-    :param y:
-        y data.
-    :type y: Iterable
-
-    :return:
-        A function that interpolate a cloud of points.
-    :rtype: scipy.interpolate.InterpolatedUnivariateSpline
-    """
-    import schedula.utils as dsp_utl
-    import scipy.interpolate as sci_itp
-
-    p = np.asarray(x)
-    v = np.asarray(y)
-
-    edges, s = bin_split(p, bin_std=(0, 10))
-
-    if len(s) > 2:
-        x, y = ([0.0], [None])
-
-        for e0, e1 in dsp_utl.pairwise(edges):
-            b = (e0 <= p) & (p < e1)
-            x.append(np.mean(p[b]))
-            y.append(np.mean(v[b]))
-
-        y[0] = y[1]
-        x.append(x[-1])
-        # noinspection PyTypeChecker,PyTypeChecker
-        y.append(y[-1] * 1.1)
-    else:
-        x, y = ([0, 1], [np.mean(y)] * 2)
-
-    return sci_itp.InterpolatedUnivariateSpline(x, y, k=1)
 
 
 def clear_fluctuations(times, gears, dt_window):
