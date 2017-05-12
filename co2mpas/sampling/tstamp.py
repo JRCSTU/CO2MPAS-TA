JRCSTU/CO2MPAS-TA

commit e9438a89ad68476adffc467c314cc9df7edbe2fe

refact(tstamp, crypto): REWORK TPARSE, split-GPG-sigs now RAISE!
+ tstamp: better distinguish between syntax-error & verification on
tags.
+ crypto: pgp_split_sig() error-msg explain the length of failing text.
+ FINISHED WORK STARTED by e8838334, where  verify_git_signed() stopped
returning none.

co2mpas/sampling/tstamp.py
@@ -362,19 +362,33 @@ class TstampSender(TstampSpec):
     def send_timestamped_email(self, msg: Union[str, bytes], subject_suffix='', dry_run=False):
         from pprint import pformat
 
+        ## TODO: Schedula to the rescue!
+
         msg_bytes = msg if isinstance(msg, bytes) else msg.encode('utf-8')
         git_auth = crypto.get_git_auth(self.config)
-        ver = git_auth.verify_git_signed(msg_bytes)
-        verdict = None if ver is None else pformat(vars(ver))
-        if not ver:
+
+        ## Allow to skip report syntxa-errors/verification if --force,
+        #  but still report the kind of syntax/sig failure
+        #
+        try:
+            ver = git_auth.verify_git_signed(msg_bytes)
+            verdict = pformat(sorted(vars(ver).items()))
+        except Exception as ex:
+            msg = "Failed to extract signed dice-report from tstamp!\n%s" % ex
             if self.force:
-                self.log.warning("Content to timestamp failed signature verification!  %s",
-                                 verdict)
+                self.log.warning(msg)
             else:
-                raise CmdException("Content to timestamp failed signature verification!\n  %s"
-                                   % verdict)
+                raise CmdException(msg)
         else:
-            self.log.debug("Content to timestamp gets verified OK: %s" % verdict)
+            if not ver:
+                msg = "Cannot verify dice-report's signature!\n%s" % verdict
+                if self.force:
+                    self.log.warning(msg)
+                else:
+                    raise CmdException(msg)
+            else:
+                msg = "The dice-report in timestamp got verified OK: %s"
+                self.log.debug(msg % verdict)
 
         msg = self._append_tstamp_recipients(msg)
         mail = self._prepare_mail(msg, subject_suffix)
@@ -534,39 +548,61 @@ class TstampReceiver(TstampSpec):
         """
         from pprint import pformat
 
+        ## TODO: Schedula to the rescue!
+
+        stag_bytes = tag_text.encode('utf-8')
         git_auth = crypto.get_git_auth(self.config)
-        ## FIXME: Should I catch errors there?  Decision not affected!
-        tag_ver = git_auth.verify_git_signed(tag_text.encode('utf-8'))
-        tag_verdict = OrderedDict(sorted(vars(tag_ver).items()))
-        if not tag_ver:
-            ## Do not fail, it might be from an unknown sender.
-            #
-            errmsg = "Cannot verify dice-report's signature due to: %s"
-            cause = pformat(tag_verdict) if self.verbose else tag_ver.status
-            self.log.warning(errmsg, cause)
+
+        ## Allow parsing signed/unsigned reports when --force,
+        #
+        ver = None
+        try:
+            ver = git_auth.verify_git_signed(stag_bytes)
+            verdict = OrderedDict(sorted(vars(ver).items()))
+        except Exception as ex:
+            msg = "Failed to extract signed dice-report from tstamp!\n%s" % ex
+            if self.force:
+                self.log.warning(msg)
+                verdict = OrderedDict(sig=msg)
+
+                ## Fall-back assuming report was not signed at all.
+                tag = stag_bytes
+            else:
+                raise CmdException(msg)
+
+        else:
+            if not ver:
+                #
+                ## Do not fail, it might be from an unknown sender.
+
+                msg = "Cannot verify dice-report's signature!\n%s"
+                self.log.warning(msg, pformat(verdict))
+            else:
+                msg = "The dice-report in timestamp got verified OK: %s"
+                self.log.debug(msg % pformat(verdict))
+
+            tag = verdict['parts']['msg']
 
         ## Parse dice-report
         #
         from . import project
 
-        tag_csig = tag_verdict['parts']
-        tag = tag_csig['msg']
         try:
             cmsg = project._CommitMsg.parse_commit_msg(tag.decode('utf-8'))
-            tag_verdict['commit_msg'] = cmsg._asdict()
-            tag_verdict['project'] = cmsg.p
-            tag_verdict['project_source'] = 'report'
+            verdict['commit_msg'] = cmsg._asdict()
+            verdict['project'] = cmsg.p
+            verdict['project_source'] = 'report'
         except Exception as ex:
             if not self.force:
                 raise
             else:
                 self.log.error("Cannot parse dice-report due to: %s", ex)
 
-        if 'project' not in tag_verdict:
-            tag_verdict['project'] = self.scan_for_project_name(tag_text)
-            tag_verdict['project_source'] = 'grep'
+        if 'project' not in verdict:
+            verdict['project'] = self.scan_for_project_name(tag_text)
+            verdict['project_source'] = 'grep'
 
-        return tag_verdict
+        return verdict
 
     def parse_tstamp_response(self, mail_text: Text) -> int:
         ## TODO: Could use dispatcher to parse tstamp-response, if failback routes were working...

co2mpas/sampling/crypto.py
@@ -116,6 +116,8 @@ def pgp_split_clearsigned(text: str) -> Dict:
 
         return groups
 
+    raise ValueError("%i-len text is not a PGP-clear-sig!" % len(text))
+
 
 def pgp_split_sig(git_content: bytes) -> (bytes, bytes):
     """
@@ -157,7 +159,9 @@ def pgp_split_sig(git_content: bytes) -> (bytes, bytes):
     :param git_content:
             Bytes as fetched from ``git cat-file tag/commit <HASHID>``.
     :return:
-            A 2-tuple(msg, sig), None if no sig found.
+            A 2-tuple: ``(msg, sig)``
+    :raise:
+            ValueError if not a sig
 
     See: https://lists.gnupg.org/pipermail/gnupg-users/2014-August/050780.html
     """
@@ -170,6 +174,8 @@ def pgp_split_sig(git_content: bytes) -> (bytes, bytes):
             ('msg', git_content[:split_pos]),
             ('sigarmor', git_content[split_pos:]),
         ])
+
+    raise ValueError("%i-len text is not a PGP-sig!" % len(git_content))
 
 
 class GpgSpec(baseapp.Spec):

tests/sampling/test_crypto.py
@@ -351,26 +351,32 @@ class TGpgSpec(unittest.TestCase):
     def test_parse_clearsigned(self, case):
         exp_msg, clearsigned = case
 
-        csig = crypto.pgp_split_clearsigned(clearsigned)
         if isinstance(exp_msg, str):
+            csig = crypto.pgp_split_clearsigned(clearsigned)
             self.assertIsInstance(csig, dict)
             self.assertEqual(len(csig), 4)
             self.assertEqual(csig['msg'], exp_msg)
             self.assertIsNotNone(csig['sigarmor'])
         else:
-            self.assertIsNone(csig)
+            with self.assertRaisesRegex(
+                    ValueError,
+                    "-len text is not a PGP-clear-sig!") as exmsg:
+                crypto.pgp_split_clearsigned(clearsigned)
 
         ## Check with \r\n at the end.
         #
         clearsigned = re.sub('$\n^', '\r\n', clearsigned, re.MULTILINE)
-        csig = crypto.pgp_split_clearsigned(clearsigned)
         if isinstance(exp_msg, str):
+            csig = crypto.pgp_split_clearsigned(clearsigned)
             self.assertIsInstance(csig, dict)
             self.assertEqual(len(csig), 4)
             self.assertEqual(csig['msg'], re.sub('$\n^', '\r\n', exp_msg), re.MULTILINE)
             self.assertIsNotNone(csig['sigarmor'])
         else:
-            self.assertIsNone(csig)
+            with self.assertRaisesRegex(
+                    ValueError,
+                    "-len text is not a PGP-clear-sig!") as exmsg:
+                crypto.pgp_split_clearsigned(clearsigned)
 
     def test_parse_git_tag_unknown_pubkey(self):
         import git

tests/sampling/test_tstamp.py
@@ -587,7 +587,8 @@ class TRX(unittest.TestCase):
 
     def test_send_timestamp(self):
         snd = tstamp.TstampSender(config=self.cfg)
-        ex_msg = r"Content to timestamp failed signature verification!\s+None"
+        ex_msg = ("Failed to extract signed dice-report from tstamp!\\s+"
+                  "0-len text is not a PGP-sig!")
         with self.assertRaisesRegex(tstamp.CmdException, ex_msg):
             snd.send_timestamped_email("", dry_run=True)
 

commit 2e0f56a24c8795b3281a5fc341ae055c85e48995

FIX(tstamp): was OVERWRITTING TSTAMP BODY tparse rework since 1.6.0.b0
+ Regression introduced in e9438a89a.

co2mpas/sampling/tstamp.py
@@ -386,21 +386,21 @@ class TstampSender(TstampSpec):
             ver = git_auth.verify_git_signed(msg_bytes)
             verdict = _mydump(sorted(vars(ver).items()))
         except Exception as ex:
-            msg = "Failed to extract signed dice-report from tstamp!\n%s" % ex
+            err = "Failed to extract signed dice-report from tstamp!\n%s" % ex
             if self.force:
-                self.log.warning(msg)
+                self.log.warning(err)
             else:
-                raise CmdException(msg)
+                raise CmdException(err)
         else:
             if not ver:
-                msg = "Cannot verify dice-report's signature!\n%s" % verdict
+                err = "Cannot verify dice-report's signature!\n%s" % verdict
                 if self.force:
-                    self.log.warning(msg)
+                    self.log.warning(err)
                 else:
-                    raise CmdException(msg)
+                    raise CmdException(err)
             else:
-                msg = "The dice-report in timestamp got verified OK: %s"
-                self.log.debug(msg, verdict)
+                err = "The dice-report in timestamp got verified OK: %s"
+                self.log.debug(err, verdict)
 
         msg = self._append_tstamp_recipients(msg)
         mail = self._prepare_mail(msg, subject_suffix)