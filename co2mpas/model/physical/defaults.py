ae415272a415da2a579ef470a34a87463f6ba2d0
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-06-17 21:34:28 +0200

    feat(co2): INTERSPERSE HotCold OPTS every (eg) 3 perts

--- co2mpas/model/physical/defaults.py
+++ co2mpas/model/physical/defaults.py
@@ -299,15 +299,22 @@ class Functions(co2_utl.Constants):
         #: Number of perturbations to identify the co2_emissions [-].
         n_perturbations = 100
 
-        #: Optimze against the HOT part of the cycle? [bool or nperts]
-        #: Setting 1 runs only once, at the begining.
-        enable_first_step = 1
+        #: Optimize HOT cycle-part separately whenever mod(nperts) == 0,
+        #: or the first n times if negative.
+        #: [bool or nperts]
+        #:
+        #: Sample values:
+        #:    * 0 (False) step disabled;
+        #:    * 1 (True) runs on every perturbation;
+        #:    * 3 runs on 1st, 3rd, 6th, ...;
+        #:    * -1: run only once, at the 1st perturbation;
+        #:    * -5 run the first 5 perts and stop.
+        enable_first_step = 3
 
-        #: Optimze against the COLD part of the cycle? [bool or nperts]
-        #: Setting 1 runs only once, at the begining.
-        enable_second_step = 1
+        #: Optimize COLD cycle-part separately (see above)
+        enable_second_step = 3
 
-        #: Optimze against full cycle? [bool]
+        #: Optimize against full cycle? [bool]
         enable_third_step = True
 
         #: Use error function against co2_emissions in perturbation loop? [-]
