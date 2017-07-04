78422a02fed01839a9aaeedd5f51c1c2d6eeaeef
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-07-04 23:47:00 +0200

    chore(ver): bump 1.6.1.dev9-->dev10

--- co2mpas/_version.py
+++ co2mpas/_version.py
@@ -9,7 +9,7 @@
 
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "1.6.1.dev9"  # Also update README.rst, CHANGES.rst,
+__version__ = version = "1.6.1.dev10"  # Also update README.rst, CHANGES.rst,
 
 #: Input/Output file's version.
 __file_version__ = "2.2.6"
@@ -21,4 +21,4 @@ __dice_report_version__ = '1.0.1'
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
 #: Release date.
-__updated__ = "2017-07-02 23:35:08"
+__updated__ = "2017-07-04 22:46:52"
