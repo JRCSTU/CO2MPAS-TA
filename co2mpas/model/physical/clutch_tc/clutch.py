fix(stability), #427: Set precision of error functions to float32 to correct the stability of the solution on different computers.

co2mpas/model/physical/clutch_tc/clutch.py
@@ -175,7 +175,8 @@ def identify_clutch_window(
         model = calibrate_clutch_prediction_model(
             times, clutch_phases, accelerations, delta, velocities,
             gear_box_speeds_in, gears)
-        return np.mean(np.abs(delta - model.model(times, clutch_phases, X)))
+        res = np.mean(np.abs(delta - model.model(times, clutch_phases, X)))
+        return np.float32(res)
 
     dt = max_clutch_window_width
     Ns = int(dt / max(np.min(np.diff(times)), 0.5)) + 1

co2mpas/model/physical/engine/co2_emission.py
@@ -1788,7 +1788,7 @@ def calibrate_model_params(
     min_e_and_p = [np.inf, copy.deepcopy(params)]
 
     def error_func(params, *args, **kwargs):
-        res = error_f(params, *args, **kwargs)
+        res = np.float32(error_f(params, *args, **kwargs))
 
         if res < min_e_and_p[0]:
             min_e_and_p[0], min_e_and_p[1] = (res, copy.deepcopy(params))

co2mpas/model/physical/gear_box/at_gear.py
@@ -516,7 +516,7 @@ class CMV(collections.OrderedDict):
             speed_pred = calculate_gear_box_speeds_in(
                 g_pre, velocities, velocity_speed_ratios, stop_velocity)
 
-            return np.mean(np.abs(speed_pred - engine_speeds_out))
+            return np.float32(np.mean(np.abs(speed_pred - engine_speeds_out)))
 
         x0 = [self[0][1]].__add__(list(itertools.chain(*velocity_limits))[:-1])
 

co2mpas/model/physical/gear_box/mechanical.py
@@ -15,7 +15,6 @@ import scipy.interpolate as sci_itp
 import scipy.optimize as sci_opt
 import scipy.stats as sci_sta
 import sklearn.cluster as sk_clu
-import sklearn.metrics as sk_met
 import schedula as sh
 import co2mpas.utils as co2_utl
 import numpy as np
@@ -153,7 +152,7 @@ def _correct_gear_shifts(
 
     def err(v, r):
         v = int(v)
-        return sk_met.mean_absolute_error(ratios[slice(v - 1, v + 1, 1)], r)
+        return np.float32(np.mean(np.abs(ratios[slice(v - 1, v + 1, 1)] - r)))
 
     k = 0
     new_gears = np.zeros_like(gears)
@@ -239,7 +238,7 @@ def calculate_gear_box_speeds_from_engine_speeds(
         std = sci_sta.binned_statistic(ratio, ratio, np.std, bins)[0]
         w = sci_sta.binned_statistic(ratio, ratio, 'count', bins)[0]
 
-        return sum(std * w)
+        return np.float32(sum(std * w))
 
     dt = shift_window / 2
     shift = sci_opt.brute(error_fun, ranges=(slice(-dt, dt, 0.1),))