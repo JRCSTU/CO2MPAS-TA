e0bd5838f90eb69868a43861447d85c4b920228c
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-07-22 16:05:57 +0300

    doc(conf): add external links to pub repo

--- doc/conf.py
+++ doc/conf.py
@@ -85,6 +85,7 @@ author = 'see AUTHORS.rst'
 
 extlinks = {
     'gh': ('https://github.com/JRCSTU/co2mpas/issues/%s', '#'),
+    'ghp': ('https://github.com/JRCSTU/CO2MPAS-TA/issues/%s', '#'),
     'aio': ('https://github.com/JRCSTU/JRCSTU/allinone/%s', '#'),
     'git': ('https://github.com/JRCSTU/co2mpas/commit/%s', ''),
 }
