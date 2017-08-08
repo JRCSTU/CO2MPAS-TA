ca17ba87e0a0dabcf8bcb8686b9734bf5c723531
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-08-08 23:38:05 +0300

    feat(co2p): drv1.0.1-->2: BLANK after 1st TAG-LINE as git's conventions.

--- co2mpas/_version.py
+++ co2mpas/_version.py
@@ -17,7 +17,7 @@ __file_version__ = "2.2.7"
 #: Compatible Input file's version.
 __input_file_version__ = "2"
 
-__dice_report_version__ = '1.0.1'
+__dice_report_version__ = '1.0.2'
 __dice_stamp_version__ = '1.0.0'
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
--- co2mpas/sampling/project.py
+++ co2mpas/sampling/project.py
@@ -697,7 +697,11 @@ class Project(transitions.Machine, ProjectSpec):
             if self.dry_run:
                 self.log.warning("DRY-RUN: Not actually committed the report, "
                                  "and it is not yet signed!")
-                self.result = _mydump(report, width=self.git_desc_width)
+                rep_lines = _mydump(report, width=self.git_desc_width).split('\n')
+                ## Insert empty line afte 1st, to respect git-msg conventions.
+                rep_lines.insert(1, '')
+
+                self.result = '\n'.join(rep_lines)
 
                 return
 
