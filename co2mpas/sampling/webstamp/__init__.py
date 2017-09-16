152c1a6a9892a28619ab26c82919aae64ee13dfe
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-09-16 17:28:10 +0300

    feat(wstamp): CDN bootstrap

--- co2mpas/sampling/webstamp/__init__.py
+++ co2mpas/sampling/webstamp/__init__.py
@@ -45,6 +45,6 @@ def create_app(configfile=None):
 
     # Because we're security-conscious developers, we also hard-code disabling
     # the CDN support (this might become a default in later versions):
-    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+    #app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
     return app
--- co2mpas/sampling/webstamp/default_config.py
+++ co2mpas/sampling/webstamp/default_config.py
@@ -1,16 +1,15 @@
 # The default_config module automatically gets imported by Appconfig, if it
 # exists. See https://pypi.python.org/pypi/flask-appconfig for details.
 #
-## Copy them below in your i.e. `local_config.py`.
+# Copy them below in your i.e. `local_config.py`.
 import logging
-
-## Copy them below in your i.e. `local_config.py`.
 
 ## Note: Don't *ever* do this in a real app. A secret key should not have a
 #       default, rather the app should fail if it is missing. For the sample
 #       application, one is provided for convenience.
 #SECRET_KEY = 'devkey'
 #WTF_CSRF_ENABLED = True
+#BOOTSTRAP_SERVE_LOCAL = True
 
 #DREPORT_CC_DEFAULT = 'foo@bar; \n'
 #STAMP_RECIPIENTS_DEFAULT = ''
