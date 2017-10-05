d1c7d7504eef18b170cbab4616d9b637c185c279
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-10-05 11:41:53 +0300

    feat(wstamp): disable landing-page to avoid spam

--- co2mpas/sampling/webstamp/frontend.py
+++ co2mpas/sampling/webstamp/frontend.py
@@ -13,12 +13,12 @@ from . import forms
 frontend = flask.Blueprint('frontend', __name__)
 
 
-# Our index-page just shows a quick explanation. Check out the template
-# "templates/index.html" documentation for more details.
-@frontend.route('/')
-def index():
-    return flask.render_template('index.html')
-
+## NOTE: DISABLE LANDING PAGE to avoid spam.
+# # Our index-page just shows a quick explanation. Check out the template
+# # "templates/index.html" documentation for more details.
+# @frontend.route('/')
+# def index():
+#     return flask.render_template('index.html')
 
 ## As a method, for blueprint to access `app` & `config`.
 #  See https://stackoverflow.com/a/23037071/548792
