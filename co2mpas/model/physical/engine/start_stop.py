401af4d875e2cfc935840b216a9b762a1026c850
vincenzoarcidiacono <vinci1it2000@gmail.com>
2018-03-29 18:28:19 +0200

    feat(s/s) Add start/stop prediction model.

--- co2mpas/model/physical/engine/start_stop.py
+++ co2mpas/model/physical/engine/start_stop.py
@@ -11,12 +11,10 @@ It contains functions that model the engine start stop strategy.
 
 import sklearn.tree as sk_tree
 import sklearn.pipeline as sk_pip
-import sklearn.preprocessing as sk_prep
 import sklearn.feature_selection as sk_fsel
 import numpy as np
 import co2mpas.model.physical.defaults as defaults
 import schedula as sh
-import co2mpas.utils as co2_utl
 
 
 def identify_on_engine(
@@ -133,149 +131,234 @@ def calibrate_start_stop_model(
     soc[0], soc[1:] = state_of_charges[0], state_of_charges[:-1]
     model = StartStopModel()
     model.fit(
-        on_engine, velocities, accelerations, engine_coolant_temperatures, soc
+        on_engine, velocities, accelerations, engine_coolant_temperatures,
+        state_of_charges
     )
 
     return model
 
 
-class DefaultStartStopModel(object):
-    @staticmethod
-    def predict(X):
-        X = np.asarray(X)
-        VEL = defaults.dfl.functions.DefaultStartStopModel.stop_velocity
-        ACC = defaults.dfl.functions.DefaultStartStopModel.plateau_acceleration
-        return (X[:, 0] > VEL) | (X[:, 1] > ACC)
-
-
+# noinspection PyShadowingBuiltins,PyUnusedLocal,PyMissingOrEmptyDocstring
 class StartStopModel(object):
+    VEL = defaults.dfl.functions.StartStopModel.stop_velocity
+    ACC = defaults.dfl.functions.StartStopModel.plateau_acceleration
+
     def __init__(self):
-        self.base = self.model = DefaultStartStopModel()
+        self.simple = self.complex = self.base
 
     def __call__(self, *args, **kwargs):
-        return self.predict(*args, **kwargs)
+        return self.base(*args, **kwargs)
 
-    def fit(self, on_engine, velocities, accelerations, *args):
+    def base(self, velocity, acceleration, *args):
+        return (velocity > self.VEL) | (acceleration > self.ACC)
+
+    def fit_simple(self, on_engine, velocities, accelerations, *args):
+        model = sk_tree.DecisionTreeClassifier(
+            random_state=0, max_depth=3
+        )
+        model.fit(np.column_stack((velocities, accelerations)), on_engine)
+        predict = model.predict
+
+        def simple(velocity, acceleration, *a):
+            return predict([[velocity, acceleration]])[0]
+
+        self.simple = simple
+
+    def fit_complex(self, on_engine, velocities, accelerations,
+                    engine_coolant_temperatures, state_of_charges):
+        model = sk_tree.DecisionTreeClassifier(random_state=0, max_depth=4)
+        model = sk_pip.Pipeline([
+            ('feature_selection',
+             sk_fsel.SelectFromModel(model)),
+            ('classification', model)
+        ])
+        model.fit(np.column_stack((
+            velocities, accelerations, engine_coolant_temperatures,
+            state_of_charges
+        )), on_engine)
+        predict = model.predict
+
+        def complex(velocity, acceleration, temperature, prev_soc):
+            return predict([[velocity, acceleration, temperature, prev_soc]])[0]
+
+        self.complex = complex
+
+    def fit(self, on_engine, velocities, accelerations,
+            engine_coolant_temperatures, state_of_charges):
         if on_engine.all():
-            self.base = self.model = DefaultStartStopModel()
+            self.simple = self.complex = self.base
         else:
-            X = np.column_stack((velocities, accelerations) + args)
-            model = sk_tree.DecisionTreeClassifier(random_state=0, max_depth=4)
-            self.model = sk_pip.Pipeline([
-                ('feature_selection',
-                 sk_fsel.SelectFromModel(model)),
-                ('classification', model)
-            ])
-            self.model.fit(X, on_engine)
-            model = sk_tree.DecisionTreeClassifier(random_state=0, max_depth=3)
-            self.base = sk_pip.Pipeline([
-                ('feature_selection',
-                 sk_prep.FunctionTransformer(lambda X: X[:, :2])),
-                ('classification', model)
-            ])
-            self.base.fit(X, on_engine)
+            self.fit_simple(on_engine, velocities, accelerations)
+            self.fit_complex(on_engine, velocities, accelerations,
+                             engine_coolant_temperatures, state_of_charges)
         return self
 
-    def predict(self, times, velocities, accelerations, *args,
-                start_stop_activation_time=None, gears=None,
-                correct_start_stop_with_gears=False,
-                min_time_engine_on_after_start=0.0, has_start_stop=True,
-                use_basic_start_stop=True):
 
-        gen = map(tuple, self.yield_on_start(
-            times, velocities, accelerations, *args, gears=gears,
-            correct_start_stop_with_gears=correct_start_stop_with_gears,
-            start_stop_activation_time=start_stop_activation_time,
-            min_time_engine_on_after_start=min_time_engine_on_after_start,
-            has_start_stop=has_start_stop,
-            use_basic_start_stop=use_basic_start_stop
-        ))
-        dtype = [('on', bool), ('starts', bool)]
-        return co2_utl.fromiter(gen, dtype=dtype, count=len(times))
+# noinspection PyMissingOrEmptyDocstring
+class EngineStartStopModel:
+    key_outputs = ['on_engine', 'engine_starts']
+    types = {bool: {'on_engine', 'engine_starts'}}
 
-    def yield_on_start(self, times, velocities, accelerations, *args,
-                       start_stop_activation_time=None, gears=None,
-                       correct_start_stop_with_gears=False,
-                       min_time_engine_on_after_start=0.0,
-                       has_start_stop=True, use_basic_start_stop=True):
-        if has_start_stop:
-            to_predict = self.when_predict_on_engine(
-                times, start_stop_activation_time, gears,
-                correct_start_stop_with_gears
-            )
-            gen = self._yield_on_start(
-                times, to_predict, velocities, accelerations, *args,
-                min_time_engine_on_after_start=min_time_engine_on_after_start,
-                use_basic_start_stop=use_basic_start_stop
-            )
+    def __init__(self, start_stop_model=None, start_stop_activation_time=None,
+                 correct_start_stop_with_gears=False,
+                 min_time_engine_on_after_start=0.0, has_start_stop=True,
+                 use_basic_start_stop=True, outputs=None):
+        self.start_stop_activation_time = start_stop_activation_time
+        self.correct_start_stop_with_gears = correct_start_stop_with_gears
+        self.min_time_engine_on_after_start = min_time_engine_on_after_start
+        self.has_start_stop = has_start_stop
+        self.use_basic_start_stop = use_basic_start_stop
+        self.start_stop_model = start_stop_model
+        self._outputs = outputs or {}
+        self.outputs = None
+
+    def __call__(self, times, *args, **kwargs):
+        self.set_outputs(times.shape[0])
+        for _ in self.yield_results(times, *args, **kwargs):
+            pass
+        return sh.selector(self.key_outputs, self.outputs, output_type='list')
+
+    def set_outputs(self, n, outputs=None):
+        if outputs is None:
+            outputs = {}
+        outputs.update(self._outputs or {})
+
+        for t, names in self.types.items():
+            names = names - set(outputs)
+            if names:
+                outputs.update(zip(names, np.empty((len(names), n), dtype=t)))
+        self.outputs = outputs
+
+    def yield_results(self, times, velocities, accelerations,
+                      engine_coolant_temperatures, state_of_charges,
+                      gears=None):
+        keys = ['on_engine', 'engine_starts']
+        if self._outputs is not None and not (set(keys) - set(self._outputs)):
+            yield from zip(
+                *sh.selector(keys, self._outputs, output_type='list'))
+        elif not self.has_start_stop:
+            outputs = self.outputs
+            outputs['on_engine'][0] = True
+            yield True, False
+            for prev in enumerate(outputs['on_engine'][:-1]):
+                yield True, not prev
         else:
-            gen = self._yield_no_start_stop(times)
-        return gen
+            outputs, t_switch_on, can_off = self.outputs, times[0], False
+            base = self.start_stop_model.base
 
-    def _yield_on_start(self, times, to_predict, velocities, accelerations,
-                        *args, min_time_engine_on_after_start=0.0,
-                        use_basic_start_stop=True):
-        base = DefaultStartStopModel().predict
-        on, prev, t_switch_on, can_off = True, True, times[0], False
-        model = self.base if use_basic_start_stop else self.model
-        predict = model.predict
-        args = (times, to_predict, velocities, accelerations) + args
-        for v in np.column_stack(args):
-            t, p, X = v[0], v[1], v[2:]
-            if p and can_off and t >= t_switch_on:
-                on = (prev or base([X])[0]) and predict([X])[0]
+            if self.use_basic_start_stop:
+                predict = self.start_stop_model.simple
             else:
-                on = True
+                predict = self.start_stop_model.complex
 
-            start = prev != on and on
-            on_start = [on, start]
-            yield on_start
-            on = on_start[0]
-            if on and prev != on:
-                t_switch_on = t + min_time_engine_on_after_start
-                can_off = False
+            it = enumerate(zip(times, zip(
+                velocities, accelerations, engine_coolant_temperatures,
+                state_of_charges
+            )))
+            prev = True
+            for i, (t, v) in it:
+                if i > 0:
+                    if outputs['engine_starts'][i - 1]:
+                        t_switch_on = t + self.min_time_engine_on_after_start
+                        can_off = False
 
-            if not can_off:
-                can_off = base([X])[0]
+                    if not can_off:
+                        can_off = base(*v)
 
-            prev = on
+                    prev = outputs['on_engine'].take(i - 1, mode='clip')
 
-    @staticmethod
-    def _yield_no_start_stop(times):
-        on, prev = True, True
-        for _ in times:
-            on = True
-            start = prev != on and on
-            on_start = [on, start]
-            yield on_start
-            prev = on_start[0]
-
-    @staticmethod
-    def when_predict_on_engine(
-            times, start_stop_activation_time=None, gears=None,
-            correct_start_stop_with_gears=False):
-        to_predict = np.ones_like(times, dtype=bool)
-
-        if start_stop_activation_time is not None:
-            to_predict[times <= start_stop_activation_time] = False
-
-        if correct_start_stop_with_gears:
-            to_predict[gears > 0] = False
-
-        return to_predict
+                # noinspection PyPep8
+                on = t <= self.start_stop_activation_time \
+                     or \
+                     self.correct_start_stop_with_gears and gears[i] > 0 \
+                     or \
+                     not (can_off and t >= t_switch_on) \
+                     or \
+                     ((prev or base(*v)) and predict(*v))
+                outputs['on_engine'][i] = on
+                outputs['engine_starts'][i] = start = on and prev != on
+                yield on, start
 
 
-def predict_engine_start_stop(
-        start_stop_model, times, velocities, accelerations,
-        engine_coolant_temperatures, state_of_charges, gears,
-        correct_start_stop_with_gears, start_stop_activation_time,
-        min_time_engine_on_after_start, has_start_stop, use_basic_start_stop):
+def define_engine_start_stop_prediction_model(
+        start_stop_model, start_stop_activation_time,
+        correct_start_stop_with_gears, min_time_engine_on_after_start,
+        has_start_stop, use_basic_start_stop):
     """
-    Predicts if the engine is on and when the engine starts.
+    Defines the engine start/stop prediction model.
 
     :param start_stop_model:
         Start/stop model.
     :type start_stop_model: StartStopModel
+
+    :param start_stop_activation_time:
+        Start-stop activation time threshold [s].
+    :type start_stop_activation_time: float
+
+    :param correct_start_stop_with_gears:
+        A flag to impose engine on when there is a gear > 0.
+    :type correct_start_stop_with_gears: bool
+
+    :param min_time_engine_on_after_start:
+        Minimum time of engine on after a start [s].
+    :type min_time_engine_on_after_start: float
+
+    :param has_start_stop:
+        Does the vehicle have start/stop system?
+    :type has_start_stop: bool
+
+    :param use_basic_start_stop:
+        If True the basic start stop model is applied, otherwise complex one.
+
+        ..note:: The basic start stop model is function of velocity and
+          acceleration. While, the complex model is function of velocity,
+          acceleration, temperature, and battery state of charge.
+    :type use_basic_start_stop: bool
+
+    :return:
+        Engine start/stop prediction model.
+    :rtype: EngineStartStopModel
+    """
+    model = EngineStartStopModel(
+        start_stop_model, start_stop_activation_time,
+        correct_start_stop_with_gears, min_time_engine_on_after_start,
+        has_start_stop, use_basic_start_stop
+    )
+    return model
+
+
+def define_fake_engine_start_stop_prediction_model(on_engine, engine_starts):
+    """
+    Defines a fake engine start/stop prediction model.
+
+    :param on_engine:
+        If the engine is on [-].
+    :type on_engine: numpy.array
+
+    :param engine_starts:
+        When the engine starts [-].
+    :type engine_starts: numpy.array
+
+    :return:
+        Engine start stop prediction model.
+    :rtype: EngineStartStopModel
+    """
+    model = EngineStartStopModel(outputs={
+        'on_engine': on_engine, 'engine_starts': engine_starts
+    })
+
+    return model
+
+
+def predict_engine_start_stop(
+        start_stop_prediction_model, times, velocities, accelerations,
+        engine_coolant_temperatures, state_of_charges, gears):
+    """
+    Predicts if the engine is on and when the engine starts.
+
+    :param start_stop_prediction_model:
+        Engine start/stop prediction model.
+    :type start_stop_prediction_model: EngineStartStopModel
 
     :param times:
         Time vector [s].
@@ -305,45 +388,14 @@ def predict_engine_start_stop(
         Gear vector [-].
     :type gears: numpy.array
 
-    :param correct_start_stop_with_gears:
-        A flag to impose engine on when there is a gear > 0.
-    :type correct_start_stop_with_gears: bool
-
-    :param start_stop_activation_time:
-        Start-stop activation time threshold [s].
-    :type start_stop_activation_time: float
-
-    :param min_time_engine_on_after_start:
-        Minimum time of engine on after a start [s].
-    :type min_time_engine_on_after_start: float
-
-    :param has_start_stop:
-        Does the vehicle have start/stop system?
-    :type has_start_stop: bool
-
-    :param use_basic_start_stop:
-        If True the basic start stop model is applied, otherwise complex one.
-
-        ..note:: The basic start stop model is function of velocity and
-          acceleration. While, the complex model is function of velocity,
-          acceleration, temperature, and battery state of charge.
-    :type use_basic_start_stop: bool
-
     :return:
         If the engine is on and when the engine starts [-, -].
     :rtype: numpy.array, numpy.array
     """
 
-    on_engine, engine_starts = start_stop_model(
+    return start_stop_prediction_model(
         times, velocities, accelerations, engine_coolant_temperatures,
-        state_of_charges, gears=gears,
-        correct_start_stop_with_gears=correct_start_stop_with_gears,
-        start_stop_activation_time=start_stop_activation_time,
-        min_time_engine_on_after_start=min_time_engine_on_after_start,
-        has_start_stop=has_start_stop, use_basic_start_stop=use_basic_start_stop
-    )
-
-    return on_engine, engine_starts
+        state_of_charges, gears)
 
 
 def default_correct_start_stop_with_gears(gear_box_type):
@@ -449,12 +501,25 @@ def start_stop():
     )
 
     d.add_function(
-        function=predict_engine_start_stop,
-        inputs=['start_stop_model', 'times', 'velocities', 'accelerations',
-                'engine_coolant_temperatures', 'state_of_charges',
-                'gears', 'correct_start_stop_with_gears',
-                'start_stop_activation_time', 'min_time_engine_on_after_start',
+        function=define_engine_start_stop_prediction_model,
+        inputs=['start_stop_model', 'start_stop_activation_time',
+                'correct_start_stop_with_gears',
+                'min_time_engine_on_after_start',
                 'has_start_stop', 'use_basic_start_stop'],
+        outputs=['start_stop_prediction_model']
+    )
+
+    d.add_function(
+        function=define_fake_engine_start_stop_prediction_model,
+        inputs=['on_engine', 'engine_starts'],
+        outputs=['start_stop_prediction_model']
+    )
+
+    d.add_function(
+        function=predict_engine_start_stop,
+        inputs=['start_stop_prediction_model', 'times', 'velocities',
+                'accelerations', 'engine_coolant_temperatures',
+                'state_of_charges', 'gears'],
         outputs=['on_engine', 'engine_starts']
     )
 
