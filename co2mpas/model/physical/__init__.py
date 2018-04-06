e8944e825c30ee8b267e585af8aa3d60d0c2efd6
arcidvi <vinci1it2000@gmail.com>
2018-04-06 14:21:30 +0200

    feat(physical): Update prediction loop using all prediction models.

--- co2mpas/model/physical/__init__.py
+++ co2mpas/model/physical/__init__.py
@@ -29,66 +29,66 @@ Modules:
 """
 
 import schedula as sh
-import numpy as np
-import functools
-import co2mpas.utils as co2_utl
+
+OUTPUTS_PREDICTION_LOOP = [
+    'alternator_currents',
+    'alternator_statuses',
+    'battery_currents',
+    'state_of_charges',
+
+    'engine_coolant_temperatures',
+    'engine_speeds_out_hot',
+    'on_engine',
+    'engine_starts',
+
+    'gear_box_efficiencies',
+    'gear_box_powers_in',
+    'gear_box_speeds_in',
+    'gear_box_temperatures',
+    'gear_box_torques_in',
+    'gears',
+
+    'final_drive_speeds_in',
+    'final_drive_torques_in',
+    'final_drive_powers_in',
+
+    'wheel_powers',
+    'wheel_speeds',
+    'wheel_torques'
+]
 
 
-def predict_vehicle_electrics_and_engine_behavior(
-        electrics_model, start_stop_model, engine_temperature_regression_model,
-        initial_engine_temperature, initial_state_of_charge, idle_engine_speed,
-        times, final_drive_powers_in, gear_box_speeds_in, gear_box_powers_in,
-        velocities, accelerations, gears, start_stop_activation_time,
-        correct_start_stop_with_gears, min_time_engine_on_after_start,
-        has_start_stop, use_basic_start_stop, max_engine_coolant_temperature):
+def prediction_loop(
+        wheels_prediction_model, final_drive_prediction_model,
+        gear_box_prediction_model, engine_prediction_model,
+        electrics_prediction_model, times, velocities, accelerations,
+        motive_powers):
     """
-    Predicts alternator and battery currents, state of charge, alternator
-    status, if the engine is on and when the engine starts, the engine speed at
-    hot condition, and the engine coolant temperature.
+    Predicts vehicle time-series.
 
-    :param electrics_model:
-        Electrics model.
-    :type electrics_model: callable
+    :param wheels_prediction_model:
+        Wheels prediction model.
+    :type wheels_prediction_model: .wheels.WheelsModel
 
-    :param start_stop_model:
-        Start/stop model.
-    :type start_stop_model: StartStopModel
+    :param final_drive_prediction_model:
+        Final drive prediction model.
+    :type final_drive_prediction_model: .final_drive.FinalDriveModel
 
-    :param engine_temperature_regression_model:
-        The calibrated engine temperature regression model.
-    :type engine_temperature_regression_model: ThermalModel
+    :param gear_box_prediction_model:
+        Gear box prediction model.
+    :type gear_box_prediction_model: .gear_box.GearBoxModel
 
-    :param initial_engine_temperature:
-        Engine initial temperature [°C]
-    :type initial_engine_temperature: float
+    :param engine_prediction_model:
+        Engine prediction model.
+    :type engine_prediction_model: .engine.EngineModel
 
-    :param initial_state_of_charge:
-        Initial state of charge of the battery [%].
-
-        .. note::
-
-            `initial_state_of_charge` = 99 is equivalent to 99%.
-    :type initial_state_of_charge: float
-
-    :param idle_engine_speed:
-        Idle engine speed and its standard deviation [RPM].
-    :type idle_engine_speed: (float, float)
+    :param electrics_prediction_model:
+        Electrics prediction model.
+    :type electrics_prediction_model: .electrics.ElectricModel
 
     :param times:
         Time vector [s].
     :type times: numpy.array
-
-    :param final_drive_powers_in:
-        Final drive power in [kW].
-    :type final_drive_powers_in: numpy.array
-
-    :param gear_box_speeds_in:
-        Gear box speed vector [RPM].
-    :type gear_box_speeds_in: numpy.array
-
-    :param gear_box_powers_in:
-        Gear box power vector [kW].
-    :type gear_box_powers_in: numpy.array
 
     :param velocities:
         Velocity vector [km/h].
@@ -98,87 +98,48 @@ def predict_vehicle_electrics_and_engine_behavior(
         Acceleration vector [m/s2].
     :type accelerations: numpy.array
 
-    :param gears:
+    :param motive_powers:
         Gear vector [-].
-    :type gears: numpy.array
-
-    :param start_stop_activation_time:
-        Start-stop activation time threshold [s].
-    :type start_stop_activation_time: float
-
-    :param correct_start_stop_with_gears:
-        A flag to impose engine on when there is a gear > 0.
-    :type correct_start_stop_with_gears: bool
-
-    :param min_time_engine_on_after_start:
-        Minimum time of engine on after a start [s].
-    :type min_time_engine_on_after_start: float
-
-    :param has_start_stop:
-        Does the vehicle have start/stop system?
-    :type has_start_stop: bool
-
-    :param use_basic_start_stop:
-        If True the basic start stop model is applied, otherwise complex one.
-
-        ..note:: The basic start stop model is function of velocity and
-          acceleration. While, the complex model is function of velocity,
-          acceleration, temperature, and battery state of charge.
-    :type use_basic_start_stop: bool
-
-    :param max_engine_coolant_temperature:
-        Maximum engine coolant temperature [°C].
-    :type max_engine_coolant_temperature: float
+    :type motive_powers: numpy.array
 
     :return:
-        Alternator and battery currents, state of charge, alternator status,
-        if the engine is on and when the engine starts, the engine speed at hot
-        condition, and the engine coolant temperature.
-        [A, A, %, -, -, -, RPM, °C].
+        Vehicle time-series
     :rtype: tuple[numpy.array]
     """
-    n = len(times)
-    soc, temp = np.zeros((2, n), dtype=float)
-    soc[0], temp[0] = initial_state_of_charge, initial_engine_temperature
+    outputs = {}
+    n = times.shape[0]
+    wheels_prediction_model.set_outputs(n, outputs)
+    final_drive_prediction_model.set_outputs(n, outputs)
+    gear_box_prediction_model.set_outputs(n, outputs)
+    engine_prediction_model.set_outputs(n, outputs)
+    electrics_prediction_model.set_outputs(n, outputs)
 
-    gen = start_stop_model.yield_on_start(
-        times, velocities, accelerations, temp, soc,
-        gears=gears, start_stop_activation_time=start_stop_activation_time,
-        correct_start_stop_with_gears=correct_start_stop_with_gears,
-        min_time_engine_on_after_start=min_time_engine_on_after_start,
-        has_start_stop=has_start_stop, use_basic_start_stop=use_basic_start_stop
+    whl = wheels_prediction_model.yield_results(velocities, motive_powers)
+
+    fd = final_drive_prediction_model.yield_results(
+        outputs['gears'], outputs['wheel_speeds'], outputs['wheel_torques'],
+        outputs['wheel_powers']
     )
 
-    args = (np.ediff1d(times, to_begin=[0]), gear_box_powers_in, accelerations,
-            gear_box_speeds_in, final_drive_powers_in, times)
+    gb = gear_box_prediction_model.yield_results(
+        times, velocities, accelerations, motive_powers,
+        outputs['final_drive_speeds_in'], outputs['final_drive_powers_in']
+    )
 
-    thermal_model = functools.partial(engine_temperature_regression_model.delta,
-                                      max_temp=max_engine_coolant_temperature)
-    from .engine import calculate_engine_speeds_out_hot as eng_speed_hot
+    eng = engine_prediction_model.yield_results(
+        times, velocities, accelerations, outputs['final_drive_powers_in'],
+        outputs['gears'], outputs['gear_box_speeds_in']
+    )
 
-    def _func():
-        eng, T, e = (True, False), temp[0], (0, 0, None, soc[0])
-        # min_soc = electrics_model.alternator_status_model.min
-        for i, (on_eng, dt, p, a, s, fdp, t) in enumerate(zip(gen, *args), 1):
-            # if e[-1] < min_soc and not on_eng[0]:
-            #    on_eng[0], on_eng[1] = True, not eng[0]
+    ele = electrics_prediction_model.yield_results(
+        times, accelerations, outputs['on_engine'], outputs['engine_starts'],
+        outputs['gear_box_powers_in']
+    )
 
-            eng_s = eng_speed_hot(s, on_eng[0], idle_engine_speed)
+    for _ in zip(whl, fd, gb, eng, ele):
+        pass
 
-            T += thermal_model(dt, fdp, eng_s, a, prev_temperature=T)
-
-            eng = tuple(on_eng)
-            e = tuple(electrics_model(dt, p, a, t, *(eng + e[1:])))
-            try:
-                temp[i], soc[i] = T, e[-1]
-            except IndexError:
-                pass
-            yield e + (eng_s, T) + eng
-
-    dtype = [('alt_c', 'f'), ('alt_sts', 'f'), ('bat_c', 'f'), ('soc', 'f'),
-             ('eng_s', 'f'), ('tmp', 'f'), ('on_eng', '?'), ('eng_st', '?')]
-    k = ('alt_c', 'bat_c', 'soc', 'alt_sts', 'on_eng', 'eng_st', 'eng_s', 'tmp')
-    return co2_utl.fromiter(_func(), dtype, k, n)
+    return sh.selector(OUTPUTS_PREDICTION_LOOP, outputs, output_type='list')
 
 
 def physical():
@@ -255,7 +216,7 @@ def physical():
         outputs=(
             'r_dynamic', 'r_wheels', 'tyre_code',
             'tyre_dynamic_rolling_coefficient', 'wheel_powers', 'wheel_speeds',
-            'wheel_torques'),
+            'wheel_torques', 'wheels_prediction_model'),
         inp_weight={'r_dynamic': 3}
     )
 
@@ -274,7 +235,8 @@ def physical():
              'wheel_torques': 'final_drive_torques_out'}),
         outputs=(
             'final_drive_powers_in', 'final_drive_ratios',
-            'final_drive_speeds_in', 'final_drive_torques_in')
+            'final_drive_speeds_in', 'final_drive_torques_in',
+            'final_drive_prediction_model')
     )
 
     from .gear_box import gear_box
@@ -283,14 +245,15 @@ def physical():
         dsp_id='gear_box_model',
         dsp=gear_box(),
         inputs=(
-            'CMV', 'CMV_Cold_Hot', 'CVT', 'DT_VA', 'DT_VAP', 'DT_VAT',
-            'DT_VATP', 'GSPV', 'GSPV_Cold_Hot', 'MVL', 'accelerations',
-            'change_gear_window_width', 'cycle_type',
-            'engine_coolant_temperatures', 'engine_mass', 'engine_max_torque',
-            'engine_speed_at_max_velocity', 'engine_speeds_out',
-            'engine_thermostat_temperature', 'f0', 'final_drive_ratios',
-            'first_gear_box_ratio', 'fuel_saving_at_strategy',
-            'full_load_curve', 'gear_box_efficiency_constants',
+            'CMV', 'CMV_Cold_Hot', 'CVT', 'DTGS', 'GSPV', 'GSPV_Cold_Hot',
+            'MVL', 'accelerations', 'change_gear_window_width', 'cycle_type',
+            'engine_coolant_temperatures', 'engine_mass', 'engine_max_power',
+            'engine_max_speed', 'engine_max_torque',
+            'engine_speed_at_max_power', 'engine_speed_at_max_velocity',
+            'engine_speeds_out', 'engine_thermostat_temperature', 'f0',
+            'final_drive_ratios', 'first_gear_box_ratio',
+            'fuel_saving_at_strategy', 'full_load_curve', 'full_load_speeds',
+            'gear_box_efficiency_constants',
             'gear_box_efficiency_parameters_cold_hot', 'gear_box_ratios',
             'gear_box_temperature_references', 'gear_box_type', 'gears',
             'has_gear_box_thermal_management', 'has_torque_converter',
@@ -307,16 +270,16 @@ def physical():
              'initial_engine_temperature': 'initial_gear_box_temperature',
              'initial_temperature': 'initial_gear_box_temperature'}),
         outputs=(
-            'CMV', 'CMV_Cold_Hot', 'CVT', 'DT_VA', 'DT_VAP', 'DT_VAT',
-            'DT_VATP', 'GSPV', 'GSPV_Cold_Hot', 'MVL',
-            'engine_speed_at_max_velocity', 'equivalent_gear_box_heat_capacity',
-            'first_gear_box_ratio', 'gear_box_efficiencies',
-            'gear_box_powers_in', 'gear_box_ratios', 'gear_box_speeds_in',
-            'gear_box_temperatures', 'gear_box_torque_losses',
-            'gear_box_torques_in', 'gear_shifts', 'gears',
-            'last_gear_box_ratio', 'max_gear', 'max_speed_velocity_ratio',
-            'maximum_velocity', 'n_gears', 'specific_gear_shifting',
-            'speed_velocity_ratios', 'velocity_speed_ratios'),
+            'CMV', 'CMV_Cold_Hot', 'CVT', 'DTGS', 'GSPV', 'GSPV_Cold_Hot',
+            'MVL', 'engine_speed_at_max_velocity',
+            'equivalent_gear_box_heat_capacity', 'first_gear_box_ratio',
+            'gear_box_efficiencies', 'gear_box_powers_in', 'gear_box_ratios',
+            'gear_box_speeds_in', 'gear_box_temperatures',
+            'gear_box_torque_losses', 'gear_box_torques_in', 'gear_shifts',
+            'gears', 'last_gear_box_ratio', 'max_gear',
+            'max_speed_velocity_ratio', 'maximum_velocity', 'n_gears',
+            'specific_gear_shifting', 'speed_velocity_ratios',
+            'velocity_speed_ratios', 'gear_box_prediction_model'),
         inp_weight={'initial_temperature': 5}
     )
 
@@ -365,7 +328,7 @@ def physical():
             'alternator_initialization_time', 'alternator_nominal_power',
             'alternator_powers_demand', 'alternator_status_model',
             'alternator_statuses', 'battery_currents', 'electric_load',
-            'electrics_model', 'initial_state_of_charge',
+            'electrics_prediction_model', 'initial_state_of_charge',
             'max_battery_charging_current', 'start_demand',
             'state_of_charge_balance', 'state_of_charge_balance_window',
             'state_of_charges')
@@ -449,24 +412,19 @@ def physical():
             'optimal_efficiency', 'phases_co2_emissions',
             'phases_fuel_consumptions', 'phases_willans_factors',
             'start_stop_model', 'use_basic_start_stop', 'willans_factors',
-            'co2_rescaling_scores'),
+            'co2_rescaling_scores', 'engine_prediction_model'),
         inp_weight={'initial_temperature': 5}
     )
 
     d.add_function(
-        function=predict_vehicle_electrics_and_engine_behavior,
-        inputs=['electrics_model', 'start_stop_model',
-                'engine_temperature_regression_model',
-                'initial_engine_temperature', 'initial_state_of_charge',
-                'idle_engine_speed', 'times', 'final_drive_powers_in',
-                'gear_box_speeds_in', 'gear_box_powers_in', 'velocities',
-                'accelerations', 'gears', 'start_stop_activation_time',
-                'correct_start_stop_with_gears',
-                'min_time_engine_on_after_start', 'has_start_stop',
-                'use_basic_start_stop', 'max_engine_coolant_temperature'],
-        outputs=['alternator_currents', 'battery_currents', 'state_of_charges',
-                 'alternator_statuses', 'on_engine', 'engine_starts',
-                 'engine_speeds_out_hot', 'engine_coolant_temperatures'],
+        function=prediction_loop,
+        inputs=[
+            'wheels_prediction_model', 'final_drive_prediction_model',
+            'gear_box_prediction_model', 'engine_prediction_model',
+            'electrics_prediction_model', 'times', 'velocities',
+            'accelerations', 'motive_powers'
+        ],
+        outputs=OUTPUTS_PREDICTION_LOOP,
         weight=10
     )
 
