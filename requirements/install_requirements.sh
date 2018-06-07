f9939b177a497ace53c654cf04aba2b8f15e2bf3
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-06-07 19:37:34 +0200

    PIN DEPS VERSIONS for v1.8.1a0

--- requirements/install_requirements.sh
+++ requirements/install_requirements.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-conda install -c conda-forge scikit-learn xgboost regex pandas matplotlib
+#
+# NOTE: conda has now MKL support for `numpy`:
+#   https://stackoverflow.com/a/37224954/548792
+#
+conda install -c conda-forge \
+    numpy==1.14.3  \
+    scikit-learn==0.19.1 \
+    scipy==0.19.0 \
+    xgboost==0.72 \
+    pandas==0.23.0 \
+    matplotlib
+
 pip install -r exe.pip --no-cache-dir
