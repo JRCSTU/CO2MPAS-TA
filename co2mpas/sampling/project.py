JRCSTU/CO2MPAS-TA

commit 2e63a5a101e33b772f62e4188e3302aff9f5a543

refact(co2report): rename `get_file_infos()` --> `extract_xxx()`

co2mpas/sampling/project.py
@@ -675,7 +675,7 @@ class Project(transitions.Machine, ProjectSpec):
         #
         try:
             rep = self._report_spec()
-            rep.get_dice_report(pfiles, expected_vfid=self.pname)
+            rep.extract_dice_report(pfiles, expected_vfid=self.pname)
             ## TODO: reuse these findos later, if --report given.
         except CmdException as ex:
             msg = "Failed extracting report from %s, due to: %s"
@@ -761,7 +761,7 @@ class Project(transitions.Machine, ProjectSpec):
                           'ANEW' if self.force else '', event.kwargs)
             repspec = self._report_spec()
             pfiles = self.list_pfiles(*PFiles._fields, _as_index_paths=True)  # @UndefinedVariable
-            report = list(repspec.get_dice_report(pfiles).values())
+            report = list(repspec.extract_dice_report(pfiles).values())
 
             if self.dry_run:
                 self.log.warning("DRY-RUN: Not actually committed the report, "
@@ -780,7 +780,7 @@ class Project(transitions.Machine, ProjectSpec):
 
     def _cb_send_email(self, event):
         """
-        Triggered by `do_sendmail()` on ENTER of `sendmail` state.
+        Triggered by `extractsendmail()` on ENTER of `sendmail` state.
 
         Parses last tag and uses class:`SMTP` to send its message as email.
 
@@ -1876,7 +1876,7 @@ class InitCmd(AppendCmd):
             from . import report
 
             repspec = report.Report(config=self.config)
-            finfos = repspec.get_dice_report(pfiles)
+            finfos = repspec.extract_dice_report(pfiles)
             for fpath, data in finfos.items():
                 iokind = data['iokind']
                 if iokind in ('inp', 'out'):
@@ -2021,7 +2021,7 @@ class TsendCmd(_SubCmd):
                                % (self.name, len(args), args))
 
         proj = self.current_project
-        ok = proj.do_sendmail()
+        ok = proj.extractsendmail()
 
         return self._format_result(ok, proj.result,
                                    is_verbose=self.verbose or proj.dry_run)

co2mpas/sampling/report.py
@@ -262,8 +262,7 @@ class Report(baseapp.Spec):
 
         return rtuples
 
-    ## TODO: Rename Report to `extract_file_infos()`.
-    def get_dice_report(self, iofiles: PFiles, expected_vfid=None):
+    def extract_dice_report(self, iofiles: PFiles, expected_vfid=None):
         tuples = self._make_report_tuples_from_iofiles(iofiles, expected_vfid)
         report = OrderedDict((file_tuple[0], _report_tuple_2_dict(*file_tuple))
                              for file_tuple
@@ -383,7 +382,7 @@ class ReportCmd(baseapp.Cmd):
         repspec = Report(config=self.config)
         if self.vfids_only:
             repspec.force = True  # Irrelevant to check for mismatching VFids.
-            for fpath, data in repspec.get_dice_report(pfiles).items():
+            for fpath, data in repspec.extract_dice_report(pfiles).items():
                 if not self.verbose:
                     fpath = osp.basename(fpath)
 

commit 3e4a05659ab33070519a189fdc7882a3453fd7dd

fix(#472,co2proj) BAD search_n_replace "extractsendmai" broke tsend...
regressed by 2e63a5a1

co2mpas/sampling/project.py
@@ -782,7 +782,7 @@ class Project(transitions.Machine, ProjectSpec):
 
     def _cb_send_email(self, event):
         """
-        Triggered by `extractsendmail()` on ENTER of `sendmail` state.
+        Triggered by `do_sendmail()` on ENTER of `sendmail` state.
 
         Parses last tag and uses class:`SMTP` to send its message as email.
 
@@ -2044,7 +2044,7 @@ class TsendCmd(_SubCmd):
                                % (self.name, len(args), args))
 
         proj = self.current_project
-        ok = proj.extractsendmail()
+        ok = proj.do_sendmail()
 
         return self._format_result(ok, proj.result,
                                    is_verbose=self.verbose or proj.dry_run)