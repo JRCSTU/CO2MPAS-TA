c9cabc7b947772a3544f217033f273af51dcd1ed
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-07-06 01:49:26 +0300

    refact(dirlock): replace print-log with debug-logging, assert with raise

--- co2mpas/sampling/dirlock.py
+++ co2mpas/sampling/dirlock.py
@@ -7,6 +7,7 @@
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 from collections import defaultdict
 import contextlib
+import logging
 import os
 import sys
 import time
@@ -15,18 +16,21 @@ from typing import Text
 import os.path as osp
 
 
+
 ## Spin-loop delay before retrying `mkdir(dir)`.
 DIR_LOCK_WAIT_SEC = 2
 ## Max time to abort wit-lock.
 ABORT_SEC = 7
 
+log = logging.getLogger(__name__)
 
 @contextlib.contextmanager
 def locked_on_dir(dpath: Text,
                   lock_wait_sec=DIR_LOCK_WAIT_SEC,
                   abort_sec=ABORT_SEC):
     dirname = osp.dirname(dpath) or '.'
-    assert osp.isdir(dirname), ("Missing parent-folder!", dirname)
+    if not osp.isdir(dirname):
+        raise ValueError("Parent-folder '%s' is missing!", dirname)
 
     start_t = time.clock()
     try:
@@ -38,8 +42,7 @@ def locked_on_dir(dpath: Text,
                 elapsed_sec = (time.clock() - start_t)
                 if elapsed_sec > abort_sec:
                     raise TimeoutError(dpath)
-                print("Dirlocking '%s' for %s sec..." % (dpath, lock_wait_sec),
-                      file=sys.stderr)
+                log.debug("Dirlocking '%s' for %s sec...", dpath, lock_wait_sec)
                 time.sleep(lock_wait_sec)
 
         yield
