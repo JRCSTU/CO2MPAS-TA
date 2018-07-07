42a060e0543de685fbfb0ddde9f386c6f145a275
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-07-07 18:41:26 +0300

    refact(reqs): rename conda-reqs script to a descriptive name

---
+++ requirements/conda_requirements.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# NOTE: conda has now MKL support for `numpy`:
+#   https://stackoverflow.com/a/37224954/548792
+#
+conda install \
+    numpy==1.14.3  \
+    scikit-learn==0.19.1 \
+    scipy==1.1.0 \
+    pandas==0.23.0 \
+    matplotlib
+
+conda install -c conda-forge \
+    xgboost==0.72 \
+
+pip install -r exe.pip --no-cache-dir
--- requirements/install_requirements.sh
+++ requirements/install_requirements.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
-#
-# NOTE: conda has now MKL support for `numpy`:
-#   https://stackoverflow.com/a/37224954/548792
-#
-conda install \
-    numpy==1.14.3  \
-    scikit-learn==0.19.1 \
-    scipy==1.1.0 \
-    pandas==0.23.0 \
-    matplotlib
+echo 'DERECATION: script `requirements/install_requirements.sh` renamed as `conda_requirements.sh`!
+  Please update the name in your scripts.' > /dev/stderr
 
-conda install -c conda-forge \
-    xgboost==0.72 \
-
-pip install -r exe.pip --no-cache-dir
+"$(realpath "$(dirname "$0")")/conda_requirements.sh"
