fc781507de901d660aab1b9e6a8575f3ac47ea67
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-08-06 21:57:15 +0200

    fix(wstamp): download stamp produced BOM which failed on `tparse`
    Bitten by the default of eligrey/FileSaver.js/issues/160

--- WebStamp/webstamp/static/fileupdown.js
+++ WebStamp/webstamp/static/fileupdown.js
@@ -49,7 +49,8 @@ $(document).ready(function(event){
                     [dice_report.val()],
                     {type: "text/plain;charset=" + document.characterSet}
                 ),
-                (download_filename.val() || download_filename[0].placeholder || "stamp.txt")
+                (download_filename.val() || download_filename[0].placeholder || "stamp.txt"),
+                true // no_auto_bom
             );
         });
     }
