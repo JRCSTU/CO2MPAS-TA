5a30bd3b16aaa42717e402df6ad199cefcd05f56
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-08-21 04:34:14 +0200

    FIX(docker): build co2sim WHEEL EXTERNALLY (any python will do),
    and then simply install the wheel in seconds.
    + enh: document `build.sh` script,
      add bash-func to derive function, just for echoing it.
    + Add .dockerignore
    - Removed `setup.cfg`, assuming built from a Git-repo, OR given a manual
    version.
    
    Nevertheless, the image is huge(!) 2.7GB.

--- pCO2SIM/docker/build.sh
+++ pCO2SIM/docker/build.sh
@@ -1,7 +1,52 @@
 #!/usr/bin/env bash
+#
+# Prequisite:
+#   Any Python (even Py-2.x) with `setuptools` installed.
+#
+# SYNTAX: 
+#   build.sh [<fallback-version>  [<docker-compose-cli-arg-1>]...]
+#
+# Example:
+#   ./build.sh  1.2.3b2  --no-cache
+#
+mydir="$(dirname "$0")"
+cd "$mydir"
 
-ver=$(python ../src/co2mpas/__init__.py version)
-export co2mpas_VERSION=${ver//[+]/-}
-export CO2MPAS_TAG=vinci1it2000/co2mpas:v${ver//[+]/-}
-echo "Building image $CO2MPAS_TAG..."
-docker-compose build
+co2mpas_version() {
+    local pname="$1" userver="$2"
+
+    POLY_VERSION="$(git describe "--match=$pname-v*")"
+
+    ## Maybe we are on an r-tag?
+    #
+    if [ -z "$POLY_VERSION" ]; then
+        POLY_VERSION="$(python ../src/co2mpas/__init__.py version)"
+    fi
+
+    ## Fallback to user-provided version
+    #
+    if [ -z "$POLY_VERSION" ]; then
+        POLY_VERSION="$userver"
+    fi
+
+    POLY_VERSION="${POLY_VERSION/+/-}"
+}
+
+## Master project defining version assumed `co2sim`.
+co2mpas_version co2sim $1
+
+export CO2MPAS_TAG="vinci1it2000/co2mpas:v$POLY_VERSION"
+echo "Building co2mpas-wheel $POLY_VERSION..."
+
+## Remove any wheels & build-dir(!) from the past.
+rm -rf ../dist/* ../build/*
+## Generate wheel in my dir, to send small docker-context as possible.
+#  Wheel will derive version from Git or failback to user-version (if any).
+#  Also provide `polyversion` lib without installing it.
+PYTHONPATH=docker/pvlib.run  co2sim_VERSION="$1"  python ../setup.py bdist_wheel
+
+
+echo "Building co2mpas-image $CO2MPAS_TAG..."
+## Pass <docker-compose-cli-args>.
+shift
+docker-compose build "$@"
