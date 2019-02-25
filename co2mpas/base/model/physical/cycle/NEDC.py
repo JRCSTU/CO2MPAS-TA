7dbaaa89c5efd99d703b8b40149bc71a889aef96
vincenzoarcidiacono <vinci1it2000@gmail.com>
2019-02-25 17:01:22 +0100

    refact(co2mpas), #506: Convert NEDC model as BlueDispatcher.

--- co2mpas/base/model/physical/cycle/NEDC.py
+++ co2mpas/base/model/physical/cycle/NEDC.py
@@ -10,50 +10,120 @@
 It provides constants for the NEDC cycle.
 """
 
-import scipy.interpolate as sci_itp
 import schedula as sh
-import numpy as np
-import functools
+from ..defaults import dfl
+
+dsp = sh.Dispatcher(
+    name='NEDC cycle model',
+    description='Returns the theoretical times, velocities, and gears of NEDC.'
+)
+
+dsp.add_data(
+    'initial_temperature', dfl.values.initial_temperature_NEDC,
+    description='Initial temperature of the test cell [°C].'
+)
+dsp.add_data(
+    'max_time', dfl.values.max_time_NEDC, 5, description='Maximum time [s].',
+
+)
+dsp.add_data('k1', dfl.values.k1)
+dsp.add_data('k2', dfl.values.k2)
+dsp.add_data('k5', dfl.values.k5, 10)
+
+dsp.add_function(
+    function_id='set_max_gear_as_default_k5',
+    function=sh.bypass,
+    inputs=['max_gear'],
+    outputs=['k5']
+)
 
 
-# noinspection PyUnusedLocal
-def nedc_gears_domain(cycle_type, gear_box_type, *args):
+def nedc_gears(times, max_gear, k1=1, k2=2, k5=2):
     """
-    Returns if the cycle is NEDC and the gearbox is manual.
+    Returns the gear shifting profile according to NEDC [-].
 
-    :param cycle_type:
-        Cycle type (WLTP or NEDC).
-    :type cycle_type: str
+    :param times:
+        Time vector [s].
+    :type times: numpy.array
 
-    :param gear_box_type:
-        Gear box type (manual or automatic or cvt).
-    :type gear_box_type: str
+    :param max_gear:
+        Maximum gear of the gear box [-].
+    :type max_gear: int
+
+    :param k1:
+        K1 NEDC parameter (first or second gear) [-].
+    :type k1: int
+
+    :param k2:
+        K2 NEDC parameter (first or second gear) [-].
+    :type k2: int
+
+    :param k5:
+        K5 NEDC parameter (first or second gear) [-].
+    :type k5: int
 
     :return:
-        If the cycle is NEDC and the gearbox is manual.
-    :rtype: bool
+        Gear vector [-].
+    :rtype: numpy.array
     """
+    from scipy.interpolate import interp1d
+    eps, parts = 0.01, {}
+    # part one
+    parts[0] = parts[1] = parts[2] = parts[3] = (
+        (0, 0), (6, 0), (6, k1), (11, k1), (11, 1), (25, 1), (25, k1), (28, k1),
+        (28, 0), (44, 0), (44, k1), (49, k1), (49, 1), (55 - eps, 1),
+        (55 - eps, 2), (93, 2), (93, k2), (96, k2), (96, 0), (112, 0),
+        (112, k1), (117, k1), (117, 1), (123 - eps, 1), (123 - eps, 2),
+        (134 - eps, 2), (134 - eps, 3), (177 - eps, 3), (177 - eps, 2),
+        (185, 2), (185, k2), (188, k2), (188, 0), (195, 0)
+    )
 
-    return cycle_type == 'NEDC' and gear_box_type == 'manual'
+    # part two
+    parts[4] = (
+        (0, k1), (20, k1), (20, 1), (26 - eps, 1), (26 - eps, 2), (37 - eps, 2),
+        (37 - eps, 3), (47 - eps, 3), (47 - eps, 4), (61, 4), (61, 5), (115, 5),
+        (115, 4), (201, 4), (201, 5), (286, 5), (286, max_gear),
+        (370, max_gear), (370, k5), (380, k5), (380, 0), (400, 0)
+    )
+
+    tg = _repeat_parts(times, parts).T
+    s = interp1d(*tg, kind='nearest', assume_sorted=True)(times)
+    s[s > max_gear] = max_gear
+    return s
 
 
-# noinspection PyUnusedLocal
-def nedc_velocities_domain(cycle_type, *args):
-    """
-    Returns if the cycle is NEDC.
-
-    :param cycle_type:
-        Cycle type (WLTP or NEDC).
-    :type cycle_type: str
-
-    :return:
-        If the cycle is NEDC.
-    :rtype: bool
-    """
-
-    return cycle_type == 'NEDC'
+def is_manual(gear_box_type, *args):
+    return gear_box_type == 'manual'
 
 
+dsp.add_function(
+    function_id='nedc_gears',
+    function=sh.add_args(nedc_gears),
+    inputs=['gear_box_type', 'times', 'max_gear', 'k1', 'k2', 'k5'],
+    outputs=['gears'],
+    input_domain=is_manual
+)
+
+
+def _repeat_parts(times, parts):
+    import numpy as np
+    from functools import reduce
+    it = [v[1] for v in sorted(parts.items())]
+
+    def _func(x, y):
+        y = np.array(y)
+        y[:, 0] += x[-1][-1][0]
+        return x + (y,)
+
+    tv = np.concatenate(reduce(_func, it[1:], (it[0],)))
+    n = int(np.ceil(times[-1] / tv[-1][0])) - 1
+    if n > 0:
+        tv = np.concatenate(reduce(_func, (tv,) * n, (tv,)))
+
+    return tv
+
+
+@sh.add_function(dsp, outputs=['velocities'])
 def nedc_velocities(times, gear_box_type):
     """
     Returns the velocity profile according to NEDC and gear box type [km/h].
@@ -70,7 +140,7 @@ def nedc_velocities(times, gear_box_type):
         Velocity vector [km/h].
     :rtype: numpy.array
     """
-
+    from numpy import interp
     parts = {
         'manual':
             {
@@ -112,147 +182,4 @@ def nedc_velocities(times, gear_box_type):
 
     tv = _repeat_parts(times, parts[gear_box_type])
 
-    return np.interp(times, *tv.T)
-
-
-def nedc_gears(times, max_gear, k1=1, k2=2, k5=2):
-    """
-    Returns the gear shifting profile according to NEDC [-].
-
-    :param times:
-        Time vector [s].
-    :type times: numpy.array
-
-    :param max_gear:
-        Maximum gear of the gear box [-].
-    :type max_gear: int
-
-    :param k1:
-        K1 NEDC parameter (first or second gear) [-].
-    :type k1: int
-
-    :param k2:
-        K2 NEDC parameter (first or second gear) [-].
-    :type k2: int
-
-    :param k5:
-        K5 NEDC parameter (first or second gear) [-].
-    :type k5: int
-
-    :return:
-        Gear vector [-].
-    :rtype: numpy.array
-    """
-    eps, parts = 0.01, {}
-    # part one
-    parts[0] = parts[1] = parts[2] = parts[3] = (
-        (0, 0), (6, 0), (6, k1), (11, k1), (11, 1), (25, 1), (25, k1), (28, k1),
-        (28, 0), (44, 0), (44, k1), (49, k1), (49, 1), (55 - eps, 1),
-        (55 - eps, 2), (93, 2), (93, k2), (96, k2), (96, 0), (112, 0),
-        (112, k1), (117, k1), (117, 1), (123 - eps, 1), (123 - eps, 2),
-        (134 - eps, 2), (134 - eps, 3), (177 - eps, 3), (177 - eps, 2),
-        (185, 2), (185, k2), (188, k2), (188, 0), (195, 0)
-    )
-
-    # part two
-    parts[4] = (
-        (0, k1), (20, k1), (20, 1), (26 - eps, 1), (26 - eps, 2), (37 - eps, 2),
-        (37 - eps, 3), (47 - eps, 3), (47 - eps, 4), (61, 4), (61, 5), (115, 5),
-        (115, 4), (201, 4), (201, 5), (286, 5), (286, max_gear),
-        (370, max_gear), (370, k5), (380, k5), (380, 0), (400, 0)
-    )
-
-    tg = _repeat_parts(times, parts).T
-    s = sci_itp.interp1d(*tg, kind='nearest', assume_sorted=True)(times)
-    s[s > max_gear] = max_gear
-
-    return s
-
-
-def _repeat_parts(times, parts):
-    it = [v[1] for v in sorted(parts.items())]
-
-    def _func(x, y):
-        y = np.array(y)
-        y[:, 0] += x[-1][-1][0]
-        return x + (y,)
-
-    tv = np.concatenate(functools.reduce(_func, it[1:], (it[0],)))
-    n = int(np.ceil(times[-1] / tv[-1][0])) - 1
-    if n > 0:
-        tv = np.concatenate(functools.reduce(_func, (tv,) * n, (tv,)))
-
-    return tv
-
-
-def nedc_cycle():
-    """
-    Defines the wltp cycle model.
-
-    .. dispatcher:: d
-
-        >>> d = nedc_cycle()
-
-    :return:
-        The wltp cycle model.
-    :rtype: schedula.Dispatcher
-    """
-
-    d = sh.Dispatcher(
-        name='NEDC cycle model',
-        description='Returns the theoretical times, velocities, and gears of '
-                    'NEDC.'
-    )
-
-    from ..defaults import dfl
-    d.add_data(
-        data_id='initial_temperature',
-        default_value=dfl.values.initial_temperature_NEDC,
-        description='Initial temperature of the test cell [°C].'
-    )
-
-    d.add_data(
-        data_id='max_time',
-        default_value=dfl.values.max_time_NEDC,
-        description='Maximum time [s].',
-        initial_dist=5
-    )
-
-    d.add_data(
-        data_id='k1',
-        default_value=dfl.values.k1
-    )
-
-    d.add_data(
-        data_id='k2',
-        default_value=dfl.values.k2
-    )
-
-    d.add_function(
-        function_id='set_max_gear_as_default_k5',
-        function=sh.bypass,
-        inputs=['max_gear'],
-        outputs=['k5']
-    )
-
-    d.add_data(
-        data_id='k5',
-        default_value=dfl.values.k5,
-        initial_dist=10
-    )
-
-    d.add_function(
-        function_id='nedc_gears',
-        function=sh.add_args(nedc_gears),
-        inputs=['gear_box_type', 'times', 'max_gear', 'k1', 'k2', 'k5'],
-        outputs=['gears'],
-        input_domain=lambda *args: args[0] == 'manual'
-    )
-
-    d.add_function(
-        function=nedc_velocities,
-        inputs=['times', 'gear_box_type'],
-        outputs=['velocities']
-    )
-
-    return d
+    return interp(times, *tv.T)
