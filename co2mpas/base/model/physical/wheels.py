77054484387532d77b9ec09c5242fedd2c832c23
vincenzoarcidiacono <vinci1it2000@gmail.com>
2019-02-25 18:13:59 +0100

    refact(co2mpas), #506: Convert wheels model as BlueDispatcher.

--- co2mpas/base/model/physical/wheels.py
+++ co2mpas/base/model/physical/wheels.py
@@ -4,22 +4,18 @@
 # Licensed under the EUPL (the 'Licence');
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
-
 """
 It contains functions that model the basic mechanics of the wheels.
 """
 
 import math
-import numpy as np
-import schedula as sh
-import co2mpas.utils as co2_utl
-from .gear_box import mechanical as gb_mec
-# noinspection PyCompatibility
 import regex
-import logging
-import schema
+import schedula as sh
+from .defaults import dfl
 
-log = logging.getLogger(__name__)
+dsp = sh.BlueDispatcher(
+    name='Wheel model', description='It models the wheel dynamics.'
+)
 
 
 def calculate_wheel_power(velocities, accelerations, road_loads, vehicle_mass):
@@ -57,6 +53,7 @@ def calculate_wheel_power(velocities, accelerations, road_loads, vehicle_mass):
 
 
 # noinspection PyIncorrectDocstring,SpellCheckingInspection
+@sh.add_function(dsp, outputs=['wheel_torques'])
 def calculate_wheel_torques(wheel_powers, wheel_speeds, coef=30000 / math.pi):
     """
     Calculates torque at the wheels [N*m].
@@ -73,10 +70,12 @@ def calculate_wheel_torques(wheel_powers, wheel_speeds, coef=30000 / math.pi):
         Torque at the wheels [N*m].
     :rtype: numpy.array | float
     """
+    import numpy as np
     with np.errstate(divide='ignore', invalid='ignore'):
         return np.where(wheel_speeds, wheel_powers / wheel_speeds * coef, 0)
 
 
+@sh.add_function(dsp, outputs=['wheel_powers'])
 def calculate_wheel_powers(wheel_torques, wheel_speeds):
     """
     Calculates power at the wheels [kW].
@@ -106,6 +105,7 @@ def _compile_speed_function(r_dynamic):
     return _wheel_speed
 
 
+@sh.add_function(dsp, outputs=['wheel_speeds'])
 def calculate_wheel_speeds(velocities, r_dynamic):
     """
     Calculates rotating speed of the wheels [RPM].
@@ -125,6 +125,45 @@ def calculate_wheel_speeds(velocities, r_dynamic):
     return _compile_speed_function(r_dynamic)(velocities)
 
 
+@sh.add_function(dsp, outputs=['r_dynamic'])
+def identify_r_dynamic(
+        velocity_speed_ratios, gear_box_ratios, final_drive_ratios):
+    """
+    Identifies the dynamic radius of the wheels [m].
+
+    :param velocity_speed_ratios:
+        Constant velocity speed ratios of the gear box [km/(h*RPM)].
+    :type velocity_speed_ratios: dict[int | float]
+
+    :param gear_box_ratios:
+        Gear box ratios [-].
+    :type gear_box_ratios: dict[int | float]
+
+    :param final_drive_ratios:
+        Final drive ratios [-].
+    :type final_drive_ratios: dict[int | float]
+
+    :return:
+        Dynamic radius of the wheels [m].
+    :rtype: float
+    """
+    from co2mpas.utils import reject_outliers
+    from .gear_box.mechanical import calculate_speed_velocity_ratios
+    svr = calculate_speed_velocity_ratios(
+        gear_box_ratios, final_drive_ratios, 1
+    )
+
+    r = [svr[k] * vs for k, vs in velocity_speed_ratios.items() if k]
+
+    r_dynamic = reject_outliers(r)[0]
+
+    return r_dynamic
+
+
+dsp.add_data('stop_velocity', dfl.values.stop_velocity)
+
+
+@sh.add_function(dsp, outputs=['r_dynamic'], weight=10)
 def identify_r_dynamic_v1(
         velocities, gears, engine_speeds_out, gear_box_ratios,
         final_drive_ratios, stop_velocity):
@@ -159,7 +198,9 @@ def identify_r_dynamic_v1(
         Dynamic radius of the wheels [m].
     :rtype: float
     """
-
+    import numpy as np
+    from co2mpas.utils import reject_outliers
+    from .gear_box import mechanical as gb_mec
     svr = gb_mec.calculate_speed_velocity_ratios(
         gear_box_ratios, final_drive_ratios, 1.0)
 
@@ -171,11 +212,15 @@ def identify_r_dynamic_v1(
     with np.errstate(divide='ignore', invalid='ignore'):
         r_dynamic = speed_x_r_dyn_ratios / engine_speeds_out
     r_dynamic = r_dynamic[~np.isnan(r_dynamic)]
-    r_dynamic = co2_utl.reject_outliers(r_dynamic)[0]
-
+    r_dynamic = reject_outliers(r_dynamic)[0]
     return r_dynamic
 
 
+dsp.add_data('plateau_acceleration', dfl.values.plateau_acceleration)
+dsp.add_data('change_gear_window_width', dfl.values.change_gear_window_width)
+
+
+@sh.add_function(dsp, outputs=['r_dynamic'], weight=11)
 def identify_r_dynamic_v2(
         times, velocities, accelerations, r_wheels, engine_speeds_out,
         gear_box_ratios, final_drive_ratios, idle_engine_speed, stop_velocity,
@@ -231,7 +276,7 @@ def identify_r_dynamic_v2(
         Dynamic radius of the wheels [m].
     :rtype: float
     """
-
+    from .gear_box import mechanical as gb_mec
     svr = gb_mec.calculate_speed_velocity_ratios(
         gear_box_ratios, final_drive_ratios, r_wheels
     )
@@ -248,79 +293,6 @@ def identify_r_dynamic_v2(
     )
 
     return r_dynamic
-
-
-def identify_r_dynamic(
-        velocity_speed_ratios, gear_box_ratios, final_drive_ratios):
-    """
-    Identifies the dynamic radius of the wheels [m].
-
-    :param velocity_speed_ratios:
-        Constant velocity speed ratios of the gear box [km/(h*RPM)].
-    :type velocity_speed_ratios: dict[int | float]
-
-    :param gear_box_ratios:
-        Gear box ratios [-].
-    :type gear_box_ratios: dict[int | float]
-
-    :param final_drive_ratios:
-        Final drive ratios [-].
-    :type final_drive_ratios: dict[int | float]
-
-    :return:
-        Dynamic radius of the wheels [m].
-    :rtype: float
-    """
-
-    svr = gb_mec.calculate_speed_velocity_ratios(
-        gear_box_ratios, final_drive_ratios, 1
-    )
-
-    r = [svr[k] * vs for k, vs in velocity_speed_ratios.items() if k]
-
-    r_dynamic = co2_utl.reject_outliers(r)[0]
-
-    return r_dynamic
-
-
-def identify_tyre_dynamic_rolling_coefficient(r_wheels, r_dynamic):
-    """
-    Identifies the dynamic rolling coefficient [-].
-
-    :param r_wheels:
-        Radius of the wheels [m].
-    :type r_wheels: float
-
-    :param r_dynamic:
-        Dynamic radius of the wheels [m].
-    :type r_dynamic: float
-
-    :return:
-        Dynamic rolling coefficient [-].
-    :rtype: float
-    """
-
-    return r_dynamic / r_wheels
-
-
-def calculate_r_dynamic(r_wheels, tyre_dynamic_rolling_coefficient):
-    """
-    Calculates the dynamic radius of the wheels [m].
-
-    :param r_wheels:
-        Radius of the wheels [m].
-    :type r_wheels: float
-
-    :param tyre_dynamic_rolling_coefficient:
-        Dynamic rolling coefficient [-].
-    :type tyre_dynamic_rolling_coefficient: float
-
-    :return:
-        Dynamic radius of the wheels [m].
-    :rtype: float
-    """
-
-    return tyre_dynamic_rolling_coefficient * r_wheels
 
 
 _re_tyre_code_iso = regex.compile(
@@ -352,6 +324,53 @@ _re_tyre_code_numeric = regex.compile(
     """, regex.IGNORECASE | regex.X | regex.DOTALL)
 
 
+def _format_tyre_dimensions(tyre_dimensions):
+    import schema
+    frt = schema.Schema({
+        schema.Optional('additional_marks'): schema.Use(str),
+        'aspect_ratio': schema.Use(float),
+        schema.Optional('carcass'): schema.Use(str),
+        'rim_diameter': schema.Use(float),
+        schema.Optional('diameter'): schema.Use(float),
+        schema.Optional('load_index'): schema.Use(str),
+        schema.Optional('load_range'): schema.Use(str),
+        'nominal_section_width': schema.Use(float),
+        schema.Optional('speed_rating'): schema.Use(str),
+        schema.Optional('use'): schema.Use(str),
+        schema.Optional('code'): schema.Use(str),
+    })
+    m = {k: v for k, v in tyre_dimensions.items() if v is not None}
+    return frt.validate(m)
+
+
+@sh.add_function(dsp, outputs=['tyre_dimensions'])
+def calculate_tyre_dimensions(tyre_code):
+    """
+    Calculates the tyre dimensions from the tyre code.
+
+    :param tyre_code:
+        Tyre code (e.g.,P225/70R14).
+    :type tyre_code: str
+
+    :return:
+        Tyre dimensions.
+    :rtype: dict
+    """
+    import schema
+    it = ('iso', _re_tyre_code_iso), ('numeric', _re_tyre_code_numeric)
+    for c, _r in it:
+        try:
+            m = _r.match(tyre_code).groupdict()
+            m['code'] = c
+            if c == 'numeric' and 'aspect_ratio' not in m:
+                b = m['nominal_section_width'].split('.')[-1][-1] == '5'
+                m['aspect_ratio'] = '82' if b else '92'
+            return _format_tyre_dimensions(m)
+        except (AttributeError, schema.SchemaError):
+            pass
+    raise ValueError('Invalid tyre code: %s', tyre_code)
+
+
 # noinspection PyUnusedLocal
 def _format_tyre_code(
         nominal_section_width, rim_diameter, aspect_ratio=0, use='', carcass='',
@@ -377,24 +396,7 @@ def _format_tyre_code(
     return ' '.join(p for p in parts if p)
 
 
-def _format_tyre_dimensions(tyre_dimensions):
-    frt = schema.Schema({
-        schema.Optional('additional_marks'): schema.Use(str),
-        'aspect_ratio': schema.Use(float),
-        schema.Optional('carcass'): schema.Use(str),
-        'rim_diameter': schema.Use(float),
-        schema.Optional('diameter'): schema.Use(float),
-        schema.Optional('load_index'): schema.Use(str),
-        schema.Optional('load_range'): schema.Use(str),
-        'nominal_section_width': schema.Use(float),
-        schema.Optional('speed_rating'): schema.Use(str),
-        schema.Optional('use'): schema.Use(str),
-        schema.Optional('code'): schema.Use(str),
-    })
-    m = {k: v for k, v in tyre_dimensions.items() if v is not None}
-    return frt.validate(m)
-
-
+@sh.add_function(dsp, outputs=['tyre_code'])
 def define_tyre_code(tyre_dimensions):
     """
     Returns the tyre code from the tyre dimensions.
@@ -413,34 +415,7 @@ def define_tyre_code(tyre_dimensions):
     return _format_tyre_code(**tyre_dimensions)
 
 
-def calculate_r_wheels(tyre_dimensions):
-    """
-    Calculates the radius of the wheels [m] from the tyre dimensions.
-
-    :param tyre_dimensions:
-        Tyre dimensions.
-
-        .. note:: The fields are : use, nominal_section_width, aspect_ratio,
-           carcass, diameter, load_index, speed_rating, and additional_marks.
-    :type tyre_dimensions: dict
-
-    :return:
-        Radius of the wheels [m].
-    :rtype: float
-    """
-    if 'diameter' in tyre_dimensions:
-        return tyre_dimensions['diameter'] * 0.0254  # Diameter is in inches.
-    a = tyre_dimensions['aspect_ratio'] / 100  # Aspect ratio is Height/Width.
-    w = tyre_dimensions['nominal_section_width']
-    if tyre_dimensions.get('code', 'iso') == 'iso':
-        w /= 1000  # Width is in mm.
-    else:
-        w *= 0.0254  # Width is in inches.
-
-    dr = tyre_dimensions['rim_diameter'] * 0.0254  # Rim is in inches.
-    return a * w + dr / 2
-
-
+@sh.add_function(dsp, outputs=['tyre_code'], weight=5)
 def default_tyre_code(r_dynamic):
     """
     Return one of the most popular tyre code according to the r dynamic.
@@ -474,30 +449,86 @@ def default_tyre_code(r_dynamic):
     return min(pop, key=_key_func)
 
 
-def calculate_tyre_dimensions(tyre_code):
+@sh.add_function(dsp, outputs=['r_wheels'])
+def calculate_r_wheels(tyre_dimensions):
     """
-    Calculates the tyre dimensions from the tyre code.
+    Calculates the radius of the wheels [m] from the tyre dimensions.
 
-    :param tyre_code:
-        Tyre code (e.g.,P225/70R14).
-    :type tyre_code: str
+    :param tyre_dimensions:
+        Tyre dimensions.
+
+        .. note:: The fields are : use, nominal_section_width, aspect_ratio,
+           carcass, diameter, load_index, speed_rating, and additional_marks.
+    :type tyre_dimensions: dict
 
     :return:
-        Tyre dimensions.
-    :rtype: dict
+        Radius of the wheels [m].
+    :rtype: float
     """
-    it = ('iso', _re_tyre_code_iso), ('numeric', _re_tyre_code_numeric)
-    for c, _r in it:
-        try:
-            m = _r.match(tyre_code).groupdict()
-            m['code'] = c
-            if c == 'numeric' and 'aspect_ratio' not in m:
-                b = m['nominal_section_width'].split('.')[-1][-1] == '5'
-                m['aspect_ratio'] = '82' if b else '92'
-            return _format_tyre_dimensions(m)
-        except (AttributeError, schema.SchemaError):
-            pass
-    raise ValueError('Invalid tyre code: %s', tyre_code)
+    if 'diameter' in tyre_dimensions:
+        return tyre_dimensions['diameter'] * 0.0254  # Diameter is in inches.
+    a = tyre_dimensions['aspect_ratio'] / 100  # Aspect ratio is Height/Width.
+    w = tyre_dimensions['nominal_section_width']
+    if tyre_dimensions.get('code', 'iso') == 'iso':
+        w /= 1000  # Width is in mm.
+    else:
+        w *= 0.0254  # Width is in inches.
+
+    dr = tyre_dimensions['rim_diameter'] * 0.0254  # Rim is in inches.
+    return a * w + dr / 2
+
+
+dsp.add_data(
+    data_id='tyre_dynamic_rolling_coefficient', initial_dist=50,
+    default_value=dfl.values.tyre_dynamic_rolling_coefficient,
+)
+
+
+@sh.add_function(dsp, outputs=['r_dynamic'])
+def calculate_r_dynamic(r_wheels, tyre_dynamic_rolling_coefficient):
+    """
+    Calculates the dynamic radius of the wheels [m].
+
+    :param r_wheels:
+        Radius of the wheels [m].
+    :type r_wheels: float
+
+    :param tyre_dynamic_rolling_coefficient:
+        Dynamic rolling coefficient [-].
+    :type tyre_dynamic_rolling_coefficient: float
+
+    :return:
+        Dynamic radius of the wheels [m].
+    :rtype: float
+    """
+
+    return tyre_dynamic_rolling_coefficient * r_wheels
+
+
+@sh.add_function(dsp, outputs=['tyre_dynamic_rolling_coefficient'])
+def identify_tyre_dynamic_rolling_coefficient(r_wheels, r_dynamic):
+    """
+    Identifies the dynamic rolling coefficient [-].
+
+    :param r_wheels:
+        Radius of the wheels [m].
+    :type r_wheels: float
+
+    :param r_dynamic:
+        Dynamic radius of the wheels [m].
+    :type r_dynamic: float
+
+    :return:
+        Dynamic rolling coefficient [-].
+    :rtype: float
+    """
+
+    return r_dynamic / r_wheels
+
+
+dsp.add_function(
+    function=sh.bypass, inputs=['motive_powers'], outputs=['wheel_powers']
+)
 
 
 # noinspection PyMissingOrEmptyDocstring
@@ -544,6 +575,7 @@ class WheelsModel:
                 yield calculate_wheel_torques(*v)
 
     def set_outputs(self, n, outputs=None):
+        import numpy as np
         if outputs is None:
             outputs = {}
         outputs.update(self._outputs or {})
@@ -573,6 +605,7 @@ class WheelsModel:
             yield s, p, t
 
 
+@sh.add_function(dsp, outputs=['wheels_prediction_model'])
 def define_fake_wheels_prediction_model(
         wheel_speeds, wheel_powers, wheel_torques):
     """
@@ -602,6 +635,7 @@ def define_fake_wheels_prediction_model(
     return model
 
 
+@sh.add_function(dsp, outputs=['wheels_prediction_model'], weight=4000)
 def define_wheels_prediction_model(r_dynamic):
     """
     Defines the wheels prediction model.
@@ -615,145 +649,3 @@ def define_wheels_prediction_model(r_dynamic):
     :rtype: WheelsModel
     """
     return WheelsModel(r_dynamic)
-
-
-def wheels():
-    """
-    Defines the wheels model.
-
-    .. dispatcher:: d
-
-        >>> d = wheels()
-
-    :return:
-        The wheels model.
-    :rtype: schedula.Dispatcher
-    """
-
-    d = sh.Dispatcher(
-        name='Wheel model',
-        description='It models the wheel dynamics.'
-    )
-
-    d.add_function(
-        function=calculate_wheel_torques,
-        inputs=['wheel_powers', 'wheel_speeds'],
-        outputs=['wheel_torques']
-    )
-
-    d.add_function(
-        function=calculate_wheel_powers,
-        inputs=['wheel_torques', 'wheel_speeds'],
-        outputs=['wheel_powers']
-    )
-
-    d.add_function(
-        function=calculate_wheel_speeds,
-        inputs=['velocities', 'r_dynamic'],
-        outputs=['wheel_speeds']
-    )
-
-    d.add_function(
-        function=identify_r_dynamic,
-        inputs=['velocity_speed_ratios', 'gear_box_ratios',
-                'final_drive_ratios'],
-        outputs=['r_dynamic']
-    )
-
-    d.add_function(
-        function=identify_r_dynamic_v1,
-        inputs=['velocities', 'gears', 'engine_speeds_out', 'gear_box_ratios',
-                'final_drive_ratios', 'stop_velocity'],
-        outputs=['r_dynamic'],
-        weight=10
-    )
-
-    from .defaults import dfl
-    d.add_data(
-        data_id='stop_velocity',
-        default_value=dfl.values.stop_velocity
-    )
-
-    d.add_data(
-        data_id='plateau_acceleration',
-        default_value=dfl.values.plateau_acceleration
-    )
-
-    d.add_data(
-        data_id='change_gear_window_width',
-        default_value=dfl.values.change_gear_window_width
-    )
-
-    d.add_function(
-        function=calculate_tyre_dimensions,
-        inputs=['tyre_code'],
-        outputs=['tyre_dimensions']
-    )
-
-    d.add_function(
-        function=calculate_r_wheels,
-        inputs=['tyre_dimensions'],
-        outputs=['r_wheels']
-    )
-
-    d.add_function(
-        function=define_tyre_code,
-        inputs=['tyre_dimensions'],
-        outputs=['tyre_code']
-    )
-
-    d.add_function(
-        function=default_tyre_code,
-        inputs=['r_dynamic'],
-        outputs=['tyre_code'],
-        weight=5
-    )
-
-    d.add_data(
-        data_id='tyre_dynamic_rolling_coefficient',
-        default_value=dfl.values.tyre_dynamic_rolling_coefficient,
-        initial_dist=50
-    )
-
-    d.add_function(
-        function=calculate_r_dynamic,
-        inputs=['r_wheels', 'tyre_dynamic_rolling_coefficient'],
-        outputs=['r_dynamic']
-    )
-
-    d.add_function(
-        function=identify_tyre_dynamic_rolling_coefficient,
-        inputs=['r_wheels', 'r_dynamic'],
-        outputs=['tyre_dynamic_rolling_coefficient']
-    )
-
-    d.add_function(
-        function=identify_r_dynamic_v2,
-        inputs=['times', 'velocities', 'accelerations', 'r_wheels',
-                'engine_speeds_out', 'gear_box_ratios', 'final_drive_ratios',
-                'idle_engine_speed', 'stop_velocity', 'plateau_acceleration',
-                'change_gear_window_width'],
-        outputs=['r_dynamic'],
-        weight=11
-    )
-
-    d.add_function(
-        function=sh.bypass,
-        inputs=['motive_powers'],
-        outputs=['wheel_powers']
-    )
-
-    d.add_function(
-        function=define_fake_wheels_prediction_model,
-        inputs=['wheel_speeds', 'wheel_powers', 'wheel_torques'],
-        outputs=['wheels_prediction_model']
-    )
-
-    d.add_function(
-        function=define_wheels_prediction_model,
-        inputs=['r_dynamic'],
-        outputs=['wheels_prediction_model'],
-        weight=4000
-    )
-
-    return d
