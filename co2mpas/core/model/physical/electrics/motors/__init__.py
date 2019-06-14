181498fe1fac0c2361074d0239ae08d88e769334
tansial <tansini.ale@gmail.com>
2019-06-14 14:45:12 +0200

    feat(motors), #516: Add p4 model.

--- co2mpas/core/model/physical/electrics/motors/__init__.py
+++ co2mpas/core/model/physical/electrics/motors/__init__.py
@@ -5,7 +5,7 @@
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 """
-Functions and a model `dsp` to model the vehicle electric motors.
+Functions and a model `dsp` to model the electric motors of the vehicle.
 
 Sub-Modules:
 
