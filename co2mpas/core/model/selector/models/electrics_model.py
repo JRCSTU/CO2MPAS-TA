9c8a69f5f791dc98e90b26e9f6fd67fb32e8f4be
vincenzoarcidiacono <vinci1it2000@gmail.com>
2019-08-05 15:44:52 +0200

    fix(selector), #516: Update selector for electrics and start/stop.

---
+++ co2mpas/core/model/selector/models/electrics_model.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015-2019 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+"""
+Functions and constants to define the alternator_model selector.
+"""
+import schedula as sh
+import co2mpas.utils as co2_utl
+from ._core import define_sub_model, _accuracy_score
+from ...physical.electrics import dsp as _electrics
+
+#: Model name.
+name = 'electrics_model'
+
+#: Parameters that constitute the model.
+models = [
+    'alternator_current_model', 'dcdc_current_model', 'has_energy_recuperation',
+    'delta_time_engine_starter', 'starter_efficiency', 'service_battery_load',
+    'service_battery_initialization_time', 'service_battery_nominal_voltage',
+    'service_battery_status_model', 'service_battery_capacity',
+    'alternator_efficiency', 'dcdc_converter_efficiency',
+    'starter_nominal_voltage', 'engine_moment_inertia',
+    'drive_battery_capacity', 'drive_battery_r0', 'drive_battery_ocv',
+    'drive_battery_n_parallel_cells', 'drive_battery_n_series_cells'
+]
+
+#: Inputs required to run the model.
+inputs = [
+    'drive_battery_electric_powers', 'times', 'motive_powers', 'accelerations',
+    'on_engine', 'starter_currents', 'initial_drive_battery_state_of_charge',
+    'initial_service_battery_state_of_charge'
+]
+
+#: Relevant outputs of the model.
+outputs = [
+    'alternator_currents', 'service_battery_currents', 'drive_battery_currents',
+    'dcdc_converter_currents', 'service_battery_state_of_charges',
+    'drive_battery_state_of_charges'
+]
+#: Targets to compare the outputs of the model.
+targets = outputs
+
+#: Weights coefficients to compute the model score.
+weights = sh.map_list(targets, 1, 1, 1, 1, 0, 0)
+
+#: Metrics to compare outputs with targets.
+metrics = sh.map_list(targets, *([co2_utl.mae] * 6))
+
+#: Upper score limits to raise the warnings.
+up_limit = dict.fromkeys(
+    ('alternator_currents', 'service_battery_currents',
+     'drive_battery_currents', 'dcdc_converter_currents'), 60
+)
+
+#: Prediction model.
+# noinspection PyProtectedMember
+dsp = sh.Blueprint(_electrics, inputs, outputs, models)._set_cls(
+    define_sub_model
+)
--- co2mpas/core/model/selector/models/start_stop_model.py
+++ co2mpas/core/model/selector/models/start_stop_model.py
@@ -9,7 +9,7 @@ Functions and constants to define the start_stop_model selector.
 """
 import schedula as sh
 from ._core import define_sub_model, _accuracy_score
-from ...physical.control import dsp as _control
+from ...physical import dsp as _physical
 
 #: Model name.
 name = 'start_stop_model'
@@ -20,7 +20,10 @@ models = [
     'motor_p3_efficiency', 'motor_p4_efficiency', 'final_drive_mean_efficiency',
     'belt_mean_efficiency', 'clutch_tc_mean_efficiency', 'motor_p2_efficiency',
     'motor_p0_efficiency', 'motor_p1_efficiency', 'gear_box_mean_efficiency',
-    'ecms_s',
+    'ecms_s', 'motor_p0_maximum_power', 'motor_p0_rated_speed',
+    'motor_p1_maximum_power', 'motor_p1_rated_speed', 'motor_p2_maximum_power',
+    'motor_p2_rated_speed', 'motor_p3_maximum_power', 'motor_p3_rated_speed',
+    'motor_p4_maximum_power', 'motor_p4_rated_speed'
 ]
 
 #: Inputs required to run the model.
@@ -32,9 +35,9 @@ inputs = [
     'motor_p1_maximum_power_function', 'motor_p0_maximum_power_function',
     'motor_p1_speed_ratio', 'motor_p0_speed_ratio', 'gear_box_speeds_in',
     'idle_engine_speed', 'motor_p4_maximum_powers', 'motor_p3_maximum_powers',
-    'motor_p2_maximum_powers', 'start_demand_function',
-    'delta_time_engine_starter', 'starter_efficiency', 'engine_moment_inertia',
-    'auxiliaries_torque_loss', 'auxiliaries_power_loss'
+    'motor_p2_maximum_powers', 'starter_model', 'engine_moment_inertia',
+    'auxiliaries_torque_loss', 'auxiliaries_power_loss',
+    'dcdc_converter_efficiency'
 ]
 
 #: Relevant outputs of the model.
@@ -54,6 +57,6 @@ dn_limit = sh.map_list(targets, 0.7, 0.7)
 
 #: Prediction model.
 # noinspection PyProtectedMember
-dsp = sh.Blueprint(_control, inputs, outputs, models)._set_cls(
+dsp = sh.Blueprint(_physical, inputs, outputs, models)._set_cls(
     define_sub_model
 )
