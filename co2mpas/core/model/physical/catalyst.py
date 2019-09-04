fix(control, catalyst), #516: Unify catalyst parameters and calculation.

co2mpas/core/model/physical/catalyst.py
@@ -131,6 +131,52 @@ def identify_catalyst_warm_up_phases(
     return phases
 
 
+@sh.add_function(dsp, outputs=['catalyst_warm_up_duration'])
+def identify_catalyst_warm_up_duration(times, catalyst_warm_up_phases):
+    """
+    Identify catalyst warm up duration [s].
+
+    :param times:
+        Time vector [s].
+    :type times: numpy.array
+
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
+
+    :return:
+        Catalyst warm up duration [s].
+    :rtype: float
+    """
+    i = co2_utl.index_phases(catalyst_warm_up_phases)
+    if i.shape[0]:
+        return float(np.mean(np.diff(times[i], axis=1)))
+    return .0
+
+
+@sh.add_function(dsp, outputs=['catalyst_cooling_duration'])
+def identify_catalyst_cooling_duration(times, catalyst_warm_up_phases):
+    """
+    Identify catalyst cooling duration [s].
+
+    :param times:
+        Time vector [s].
+    :type times: numpy.array
+
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
+
+    :return:
+        Catalyst cooling duration [s].
+    :rtype: float
+    """
+    i = co2_utl.index_phases(catalyst_warm_up_phases)
+    if i.shape[0] > 2:
+        return float(np.mean(np.diff(times[i].ravel())[1::2]))
+    return float('inf')
+
+
 @sh.add_function(dsp, outputs=['catalyst_speeds_delta'])
 def identify_catalyst_speeds_delta(
         catalyst_warm_up_phases, engine_speeds_out, engine_speeds_out_hot):
@@ -180,43 +226,6 @@ def calculate_engine_speeds_base(
     return engine_speeds_out_hot + catalyst_speeds_delta
 
 
-# noinspection PyMissingOrEmptyDocstring, PyProtectedMember
-class CatalystSpeedModel:
-    def __init__(self, model=None, warming_time=0, cooling_time=float('inf')):
-        self.model = model
-        self.cooling_time = cooling_time
-        self.warming_time = warming_time
-
-    def fit(self, times, catalyst_speeds_delta, catalyst_warm_up_phases):
-        if not catalyst_warm_up_phases.any():
-            return self
-        from sklearn.isotonic import IsotonicRegression
-        indices = co2_utl.index_phases(catalyst_warm_up_phases)
-        x, y, model = [], [], IsotonicRegression(increasing=False)
-        indices = indices[np.diff(times[indices], axis=1)[:, 0] > 5]
-        for i, j in indices:
-            x.extend(times[i:j + 1] - times[i])
-            y.extend(catalyst_speeds_delta[i:j + 1])
-        self.warming_time = np.max(x)
-        dt = np.diff(times[indices].ravel())[1::2]
-        self.cooling_time = np.mean(dt) if dt.size else float('inf')
-        # noinspection PyUnresolvedReferences
-        self.model = model.fit(x, y).predict
-        return self
-
-    def __call__(self, times, on_engine, is_warm=False):
-        speeds_delta, w_time = np.zeros_like(times, float), self.warming_time
-        if w_time and self.model:
-            indices = co2_utl.index_phases(on_engine)
-            indices = indices[np.append(not is_warm, np.diff(
-                times[indices].ravel()
-            )[1::2] > self.cooling_time)]
-            for i, j in indices:
-                t = times[i:j + 1] - times[i]
-                speeds_delta[i:j + 1] = np.where(t < w_time, self.model(t), 0)
-        return speeds_delta
-
-
 @sh.add_function(dsp, outputs=['catalyst_speed_model'])
 def calibrate_catalyst_speed_model(
         times, catalyst_warm_up_phases, catalyst_speeds_delta):
@@ -237,21 +246,34 @@ def calibrate_catalyst_speed_model(
 
     :return:
         Catalyst speed model.
-    :rtype: CatalystSpeedModel
+    :rtype: function
     """
-    return CatalystSpeedModel().fit(
-        times, catalyst_speeds_delta, catalyst_warm_up_phases
-    )
+    if catalyst_warm_up_phases.any():
+        from sklearn.isotonic import IsotonicRegression
+        indices = co2_utl.index_phases(catalyst_warm_up_phases)
+        x, y, model = [], [], IsotonicRegression(increasing=False)
+        indices = indices[np.diff(times[indices], axis=1)[:, 0] > 5]
+        for i, j in indices:
+            x.extend(times[i:j + 1] - times[i])
+            y.extend(catalyst_speeds_delta[i:j + 1])
+        # noinspection PyUnresolvedReferences
+        return model.fit(x, y).predict
 
 
-@sh.add_function(dsp, outputs=['catalyst_speeds_delta'])
-def calculate_catalyst_speeds_delta(catalyst_speed_model, times, on_engine):
+@sh.add_function(dsp, outputs=['catalyst_warm_up_phases'])
+def predict_catalyst_warm_up_phases(
+        catalyst_warm_up_duration, catalyst_cooling_duration, times, on_engine,
+        is_cycle_hot):
     """
     Calculates the engine speed delta due to the catalyst warm up [RPM].
 
-    :param catalyst_speed_model:
-        Catalyst speed model.
-    :type catalyst_speed_model: CatalystSpeedModel
+    :param catalyst_warm_up_duration:
+        Catalyst warm up duration [s].
+    :type catalyst_warm_up_duration: float
+
+    :param catalyst_cooling_duration:
+        Catalyst cooling duration [s].
+    :type catalyst_cooling_duration: float
 
     :param times:
         Time vector [s].
@@ -261,8 +283,50 @@ def calculate_catalyst_speeds_delta(catalyst_speed_model, times, on_engine):
         If the engine is on [-].
     :type on_engine: numpy.array
 
+    :param is_cycle_hot:
+        Is an hot cycle?
+    :type is_cycle_hot: bool
+
+    :return:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :rtype: numpy.array
+    """
+    phases = np.zeros_like(times, bool)
+    if catalyst_warm_up_duration:
+        indices = co2_utl.index_phases(on_engine)
+        indices = indices[np.append(not is_cycle_hot, np.diff(
+            times[indices].ravel()
+        )[1::2] > catalyst_cooling_duration)]
+        for i, j in indices:
+            t = times[i:j + 1] - times[i]
+            phases[i:j + 1] = t < catalyst_warm_up_duration
+    return phases
+
+
+@sh.add_function(dsp, outputs=['catalyst_speeds_delta'])
+def predict_catalyst_speeds_delta(
+        catalyst_speed_model, times, catalyst_warm_up_phases):
+    """
+    Predicts the engine speed delta due to the catalyst warm up [RPM].
+
+    :param catalyst_speed_model:
+        Catalyst speed model.
+    :type catalyst_speed_model: function
+
+    :param times:
+        Time vector [s].
+    :type times: numpy.array
+
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
+
     :return:
         Engine speed delta due to the catalyst warm up [RPM].
     :rtype: numpy.array
     """
-    return catalyst_speed_model(times, on_engine)
+    delta = np.zeros_like(times, float)
+    if catalyst_speed_model:
+        for i, j in co2_utl.index_phases(catalyst_warm_up_phases):
+            delta[i:j + 1] = catalyst_speed_model(times[i:j + 1] - times[i])
+    return delta

co2mpas/core/model/physical/control/hybrid.py
@@ -923,8 +923,8 @@ def calculate_ems_data(
     )
 
 
-@sh.add_function(dsp, inputs_kwargs=True, outputs=['catalyst_warm_up'])
-def identify_catalyst_warm_up(
+@sh.add_function(dsp, inputs_kwargs=True, outputs=['catalyst_warm_up_phases'])
+def identify_catalyst_warm_up_phases(
         times, engine_powers_out, engine_coolant_temperatures, on_engine,
         engine_thermostat_temperature, ems_data, hybrid_modes,
         is_cycle_hot=False):
@@ -967,61 +967,41 @@ def identify_catalyst_warm_up(
         Catalyst warm up phase.
     :rtype: numpy.array
     """
-    anomalies = np.zeros_like(times)
+    warm_up = np.zeros_like(times)
     if not on_engine.any():
-        return anomalies.astype(bool)
+        return warm_up.astype(bool)
     i = np.where(on_engine)[0]
     p = np.choose(ems_data['hybrid_modes'].ravel() - 1, [
         ems_data[k]['power_ice'].ravel() for k in ('parallel', 'serial')
     ])[i]
     with np.errstate(divide='ignore', invalid='ignore'):
         # noinspection PyUnresolvedReferences
-        anomalies[i[~co2_utl.get_inliers(
+        warm_up[i[~co2_utl.get_inliers(
             engine_powers_out[i] / p, 2, np.nanmedian, co2_utl.mad
         )[0]]] = 1
-    anomalies = co2_utl.median_filter(times, anomalies, 5)
-    anomalies = co2_utl.clear_fluctuations(times, anomalies, 5).astype(bool)
-    i, temp = co2_utl.index_phases(anomalies), engine_coolant_temperatures
-    b = np.diff(temp[i], axis=1) > 4
-    b &= temp[i[:, 0], None] < (engine_thermostat_temperature - 10)
-    b &= np.diff(times[i], axis=1) > 5
+    warm_up = co2_utl.median_filter(times, warm_up, 5)
+    warm_up = co2_utl.clear_fluctuations(times, warm_up, 5).astype(bool)
+    indices, temp = co2_utl.index_phases(warm_up), engine_coolant_temperatures
+    b = np.diff(temp[indices], axis=1) > 4
+    b &= temp[indices[:, 0], None] < (engine_thermostat_temperature - 10)
+    b &= np.diff(times[indices], axis=1) > 5
     b &= np.apply_along_axis(
-        lambda a: np.in1d(2, hybrid_modes[slice(*a)]), 1, i
+        lambda a: np.in1d(2, hybrid_modes[slice(*a)]), 1, indices
     )
-    if is_cycle_hot:
-        t0 = times[0] + dfl.functions.identify_catalyst_warm_up.cooling_time
-        b &= times[i[:, 0]] > t0
-    i = i[b.ravel()]
-    anomalies[:] = False
-    if i.shape[0]:
-        i, j = i[0]
-        while i and hybrid_modes.take(i - 1, mode='clip'):
-            i -= 1
-        anomalies[i:j + 1] = True
-    return anomalies
+    t_cooling = dfl.functions.identify_catalyst_warm_up_phases.cooling_time
+    t0 = is_cycle_hot and times[0] + t_cooling or 0
+    warming_phases = np.zeros_like(on_engine, bool)
+    for i, j in co2_utl.index_phases(on_engine):
+        warming_phases[i:j + 1] = times[i] >= t0
+        t0 = times[j] + t_cooling
 
-
-@sh.add_function(dsp, outputs=['catalyst_warm_up_duration'])
-def identify_catalyst_warm_up_duration(times, catalyst_warm_up):
-    """
-    Identify catalyst warm up duration [s].
-
-    :param times:
-        Time vector [s].
-    :type times: numpy.array
-
-    :param catalyst_warm_up:
-        Catalyst warm up phase.
-    :type catalyst_warm_up: numpy.array
-
-    :return:
-        Catalyst warm up duration [s].
-    :rtype: float
-    """
-    i = co2_utl.index_phases(catalyst_warm_up)
-    if i.shape[0]:
-        return float(np.mean(np.diff(times[i], axis=1)))
-    return .0
+    warm_up[:] = False
+    for i, j in indices[b.ravel()]:
+        if warming_phases[i]:
+            while i and warming_phases.take(i - 1, mode='clip'):
+                i -= 1
+            warm_up[i:j + 1] = True
+    return warm_up
 
 
 # noinspection PyMissingOrEmptyDocstring
@@ -1030,12 +1010,13 @@ class StartStopHybrid:
         self.params = params
 
     def fit(self, ems_data, on_engine, drive_battery_state_of_charges,
-            catalyst_warm_up):
+            catalyst_warm_up_phases):
         import lmfit
         k = np.where(~on_engine, *ems_data['k_reference'].T)
 
         # Filter data.
-        b = ~catalyst_warm_up & ~ems_data['force_on_engine'] & np.isfinite(k)
+        b = ~catalyst_warm_up_phases & ~ems_data['force_on_engine']
+        b &= np.isfinite(k)
         s = np.where(on_engine[b], 1, -1)
         k, soc = k[b].T, drive_battery_state_of_charges[b]
         del b
@@ -1064,7 +1045,8 @@ class StartStopHybrid:
 
 @sh.add_function(dsp, outputs=['start_stop_hybrid_params'])
 def calibrate_start_stop_hybrid_params(
-        ems_data, on_engine, drive_battery_state_of_charges, catalyst_warm_up):
+        ems_data, on_engine, drive_battery_state_of_charges,
+        catalyst_warm_up_phases):
     """
     Calibrate start stop model for hybrid electric vehicles.
 
@@ -1080,9 +1062,9 @@ def calibrate_start_stop_hybrid_params(
         State of charge of the drive battery [%].
     :type drive_battery_state_of_charges: numpy.array
 
-    :param catalyst_warm_up:
-        Catalyst warm up phase.
-    :type catalyst_warm_up: numpy.array
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
 
     :return:
         Params of start stop model for hybrid electric vehicles.
@@ -1090,7 +1072,7 @@ def calibrate_start_stop_hybrid_params(
     """
     return StartStopHybrid().fit(
         ems_data, on_engine, drive_battery_state_of_charges,
-        catalyst_warm_up
+        catalyst_warm_up_phases
     ).params
 
 
@@ -1111,14 +1093,9 @@ def define_start_stop_hybrid(start_stop_hybrid_params):
 
 
 @sh.add_function(dsp, outputs=['hybrid_modes'])
-def calculate_hybrid_modes(
-        times, ems_data, on_engine, catalyst_warm_up_duration):
+def calculate_hybrid_modes(ems_data, on_engine, catalyst_warm_up_phases):
     """
     Calculate the hybrid mode status (0: EV, 1: Parallel, 2: Serial).
-
-    :param times:
-        Time vector [s].
-    :type times: numpy.array
 
     :param ems_data:
         EMS decision data.
@@ -1128,28 +1105,26 @@ def calculate_hybrid_modes(
         If the engine is on [-].
     :type on_engine: numpy.array
 
-    :param catalyst_warm_up_duration:
-        Catalyst warm up duration [s].
-    :type catalyst_warm_up_duration: float
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
 
     :return:
         Hybrid mode status (0: EV, 1: Parallel, 2: Serial).
     :rtype: numpy.array
     """
-    modes, i = np.zeros_like(times, int), np.where(on_engine)[0]
-    if i.size:
-        modes[i] = ems_data['hybrid_modes'].ravel()[i]
-        t = times[i]
-        i = i[t <= t[0] + catalyst_warm_up_duration]
-        modes[i] = 2
+    modes = ems_data['hybrid_modes'].ravel().copy()
+    modes[catalyst_warm_up_phases] = 2
+    modes[~on_engine] = 0
     return modes
 
 
 @sh.add_function(dsp, outputs=['hybrid_modes'])
 def predict_hybrid_modes(
         start_stop_hybrid, ems_data, drive_battery_model, times, motive_powers,
-        accelerations, catalyst_warm_up_duration, start_stop_activation_time,
-        min_time_engine_on_after_start, is_cycle_hot):
+        accelerations, catalyst_warm_up_duration, catalyst_cooling_duration,
+        start_stop_activation_time, min_time_engine_on_after_start,
+        is_cycle_hot):
     """
     Predicts the hybrid mode status (0: EV, 1: Parallel, 2: Serial).
 
@@ -1180,6 +1155,10 @@ def predict_hybrid_modes(
     :param catalyst_warm_up_duration:
         Catalyst warm up duration [s].
     :type catalyst_warm_up_duration: float
+
+    :param catalyst_cooling_duration:
+        Catalyst cooling duration [s].
+    :type catalyst_cooling_duration: float
 
     :param min_time_engine_on_after_start:
         Minimum time of engine on after a start [s].
@@ -1221,7 +1200,9 @@ def predict_hybrid_modes(
         times, motive_powers, accelerations, hybrid_modes, r['k_reference'],
         r['hybrid_modes'].ravel()
     ))
-    is_warm = is_cycle_hot
+    t_catalyst = times[0]
+    if is_cycle_hot:
+        t_catalyst += catalyst_cooling_duration
     for i, (t, motive_power, acc, mode, k_ref, mode_ref) in it:
         pre_mode = hybrid_modes.take(i - 1, mode='clip')
         j = int(bool(pre_mode))
@@ -1234,12 +1215,14 @@ def predict_hybrid_modes(
         if bool(pre_mode) ^ bool(mode) and i:
             if mode:
                 t0 = t + min_time_engine_on_after_start
-                if not is_warm:
+                if t >= t_catalyst:
                     if not hybrid_modes[i]:
                         mode = 3
                     j = np.searchsorted(times, t + catalyst_warm_up_duration)
                     j += 1
                     hybrid_modes[i:j][hybrid_modes[i:j] == 0], is_warm = 3, True
+            else:
+                t_catalyst = t + catalyst_cooling_duration
             starter_curr = starter_bat[mode][i]
         soc = drive_battery_model(
             current_bat[mode][i], t, motive_power, acc, bool(mode), starter_curr
@@ -1264,48 +1247,9 @@ def identify_on_engine(hybrid_modes):
     return hybrid_modes.astype(bool)
 
 
-@sh.add_function(dsp, inputs_kwargs=True, outputs=['catalyst_warm_up'])
-def predict_catalyst_warm_up(
-        times, on_engine, catalyst_warm_up_duration, is_cycle_hot=False):
-    """
-    Predict catalyst warm up phase.
-
-    :param times:
-        Time vector [s].
-    :type times: numpy.array
-
-    :param on_engine:
-        If the engine is on [-].
-    :type on_engine: numpy.array
-
-    :param catalyst_warm_up_duration:
-        Catalyst warm up duration [s].
-    :type catalyst_warm_up_duration: float
-
-    :param is_cycle_hot:
-        Is an hot cycle?
-    :type is_cycle_hot: bool, optional
-
-    :return:
-        Catalyst warm up phase.
-    :rtype: numpy.array
-    """
-    res = np.zeros_like(times, bool)
-    if on_engine.any():
-        dt = dfl.functions.identify_catalyst_warm_up.cooling_time
-        tc = times[0] + (dt if is_cycle_hot else 0)
-
-        for i, j in co2_utl.index_phases(on_engine):
-            t0 = times[i]
-            if t0 >= tc:
-                res[i:j + 1] = times[i:j + 1] < t0 + catalyst_warm_up_duration
-            tc = times[j] + dt
-    return res
-
-
 @sh.add_function(dsp, outputs=['engine_speeds_out_hot'])
 def identify_engine_speeds_out_hot(
-        engine_speeds_base, catalyst_warm_up, idle_engine_speed):
+        engine_speeds_base, catalyst_warm_up_phases, idle_engine_speed):
     """
     Predicts the engine speed at hot condition [RPM].
 
@@ -1313,9 +1257,9 @@ def identify_engine_speeds_out_hot(
         Base engine speed (i.e., without clutch/TC effect) [RPM].
     :type engine_speeds_base: numpy.array
 
-    :param catalyst_warm_up:
-        Catalyst warm up phase.
-    :type catalyst_warm_up: numpy.array
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
 
     :param idle_engine_speed:
         Engine speed idle median and std [RPM].
@@ -1325,12 +1269,14 @@ def identify_engine_speeds_out_hot(
         Engine speed at hot condition [RPM].
     :rtype: numpy.array
     """
-    return np.where(catalyst_warm_up, idle_engine_speed[0], engine_speeds_base)
+    return np.where(
+        catalyst_warm_up_phases, idle_engine_speed[0], engine_speeds_base
+    )
 
 
 @sh.add_function(dsp, outputs=['engine_speeds_out_hot'], weight=1)
 def predict_engine_speeds_out_hot(
-        ems_data, hybrid_modes, catalyst_warm_up, idle_engine_speed):
+        ems_data, hybrid_modes, catalyst_warm_up_phases, idle_engine_speed):
     """
     Predicts the engine speed at hot condition [RPM].
 
@@ -1342,9 +1288,9 @@ def predict_engine_speeds_out_hot(
         Hybrid mode status (0: EV, 1: Parallel, 2: Serial).
     :type hybrid_modes: numpy.array
 
-    :param catalyst_warm_up:
-        Catalyst warm up phase.
-    :type catalyst_warm_up: numpy.array
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
 
     :param idle_engine_speed:
         Engine speed idle median and std [RPM].
@@ -1356,7 +1302,7 @@ def predict_engine_speeds_out_hot(
     """
     it = ems_data['electric'], ems_data['parallel'], ems_data['serial']
     speeds = np.choose(hybrid_modes, [d['speed_ice'].ravel() for d in it])
-    speeds[catalyst_warm_up] = idle_engine_speed[0]
+    speeds[catalyst_warm_up_phases] = idle_engine_speed[0]
     return speeds
 
 
@@ -1366,7 +1312,7 @@ def predict_engine_speeds_out_hot(
     'motor_p1_electric_powers', 'motor_p0_electric_powers'
 ])
 def predict_motors_electric_powers(
-        ems_data, catalyst_warm_up, hybrid_modes, engine_speeds_out_hot):
+        ems_data, catalyst_warm_up_phases, hybrid_modes, engine_speeds_out_hot):
     """
     Predicts motors electric power split [kW].
 
@@ -1374,9 +1320,9 @@ def predict_motors_electric_powers(
         EMS decision data.
     :type ems_data: dict
 
-    :param catalyst_warm_up:
-        Catalyst warm up phase.
-    :type catalyst_warm_up: numpy.array
+    :param catalyst_warm_up_phases:
+        Phases when engine speed is affected by the catalyst warm up [-].
+    :type catalyst_warm_up_phases: numpy.array
 
     :param hybrid_modes:
         Hybrid mode status (0: EV, 1: Parallel, 2: Serial).
@@ -1390,7 +1336,9 @@ def predict_motors_electric_powers(
         Motors electric powers [kW].
     :rtype: tuple[numpy.array]
     """
-    mode = np.where(catalyst_warm_up & (hybrid_modes == 2), 0, hybrid_modes)
+    mode = np.where(
+        catalyst_warm_up_phases & (hybrid_modes == 2), 0, hybrid_modes
+    )
     return np.choose(mode, [
         d['battery_power_split'](d['power_bat'].ravel(), engine_speeds_out_hot)
         for d in (ems_data[k] for k in ('electric', 'parallel', 'serial'))