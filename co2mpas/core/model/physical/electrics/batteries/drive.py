fix(battery): Correct calculation flow of `drive_battery_voltages`.

co2mpas/core/model/physical/electrics/batteries/drive.py
@@ -494,7 +494,7 @@ def identify_drive_battery_nominal_voltage(drive_battery_voltages):
     return np.median(drive_battery_voltages[drive_battery_voltages > dfl.EPS])
 
 
-@sh.add_function(dsp, outputs=['drive_battery_voltages'], weight=sh.inf(11, 0))
+@sh.add_function(dsp, outputs=['drive_battery_voltages'], weight=sh.inf(15, 0))
 def define_drive_battery_voltages(times, drive_battery_nominal_voltage):
     """
     Defines drive battery voltage vector [V].