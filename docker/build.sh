b14e5e31880a2fadcfe0a0e5b31d7ad615b772e9
vincenzoarcidiacono <vinci1it2000@gmail.com>
2020-03-03 12:08:34 +0100

    fix(docker): Correct build script.

--- docker/build.sh
+++ docker/build.sh
@@ -2,9 +2,10 @@
 
 cd "$(dirname "$0")"
 
-co2mpas_VERSION=$(python ../co2mpas/_version.py version)
+co2mpas_VERSION=$(python ../co2mpas/_version.py --version)
 export co2mpas_VERSION=${co2mpas_VERSION/+/-}
 export CO2MPAS_TAG_DEBIAN=vinci1it2000/co2mpas-debian:v$co2mpas_VERSION
+export CO2MPAS_TAG_EXE=vinci1it2000/co2mpas-exe:v$co2mpas_VERSION
 
 echo "Building images for CO2MPAS version: $co2mpas_VERSION..."
 docker-compose build "$@"
