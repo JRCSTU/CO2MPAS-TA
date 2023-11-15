c284b69007cb737aaf9032414bdd64542691288d
vincenzoarcidiacono <vinci1it2000@gmail.com>
2023-11-15 13:30:01 +0100

    feat(bin): Add script to plot the core model.

---
+++ bin/molde_plot.py
@@ -0,0 +1,8 @@
+#  #
+#  Copyright 2015-2023 European Commission (JRC);
+#  Licensed under the EUPL (the 'Licence');
+#  You may not use this work except in compliance with the Licence.
+#  You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+from co2mpas.core import dsp
+dsp = dsp.register()
+dsp.plot(directory='plot',render=True)
